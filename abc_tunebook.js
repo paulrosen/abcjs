0745c99692928a916fe6bf76d3f2df41a2fa6075
Paul Rosen <paul@paulrosen.net>
2009-12-16 05:15:35 +0000

    More riguous checking for X: to start a tune, and a blank line to end one. All intertune characters are thrown away. Also returns an offset into each tune.

--- abc_tunebook.js
+++ abc_tunebook.js
@@ -4,14 +4,25 @@
 
 var AbcTuneBook = Class.create({
 	initialize: function (book) {
+		var This = this;
 		book = book.strip();
-		this.tunes = book.split("\nX:");
-		if (!this.tunes[0].startsWith("X:"))
-			this.tunes[0] = "X:1\n" + this.tunes[0];	// X: is implied if it doesn't exist.
-		this.tunes.each(function(tune) {
-			tune = tune.strip();
+		var tunes = book.split("\nX:");
+		for (var i = 1; i < tunes.length; i++)	// Put back the X: that we lost when splitting the tunes.
+			tunes[i] = "X:" + tunes[i];
+		// Keep track of the character position each tune starts with.
+		var pos = 0;
+		This.tunes = [];
+		tunes.each(function(tune) {
+			This.tunes.push({ abc: tune, startPos: pos});
+			pos += tune.length;
 		});
-		for (var i = 1; i < this.tunes.length; i++)	// Put back the X: that we lost when splitting the tunes.
-			this.tunes[i] = "X:" + this.tunes[i];
+		if (This.tunes.length > 1 && !This.tunes[0].abc.startsWith('X:'))	// If there is only one tune, the X: might be missing, otherwise assume the top of the file is "intertune"
+			This.tunes.shift();
+		// Now, the tune ends at a blank line, so truncate it if needed. There may be "intertune" stuff.
+		This.tunes.each(function(tune) {
+			var end = tune.abc.indexOf('\n\n');
+			if (end > 0)
+				tune.abc = tune.abc.substring(0, end);
+		});
 	}
 });
