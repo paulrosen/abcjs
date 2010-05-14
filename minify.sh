cc52b4f126da9c3c61daabad0796dbbb9015cda4
Gregory Dyke <gregdyke@gmail.com>
2010-05-14 19:27:18 +0000

    include version name in minified js

--- minify.sh
+++ minify.sh
@@ -2,8 +2,8 @@ cat proto.js sprintf.js abc_glyphs.js abc_graphelements.js abc_layout.js abc_wri
 cat raphael.js abcjs-no-raphael.js > abcjs_all.js
 cat abcjs_all.js abc_editor.js > abcjs_editor.js
 cat abcjs_all.js abc_plugin.js > abcjs_plugin.js
-java -jar yuicompressor-2.4.2.jar  --line-break 7000 -o abcjs_basic-min.js abcjs_all.js
-java -jar yuicompressor-2.4.2.jar  --line-break 7000 -o abcjs_editor-min.js abcjs_editor.js
-java -jar yuicompressor-2.4.2.jar  --line-break 7000 -o abcjs_plugin-nojquery-min.js abcjs_plugin.js
-cat jquery-1.4.2.min.js abcjs_plugin-nojquery-min.js > abcjs_plugin-min.js
+java -jar yuicompressor-2.4.2.jar  --line-break 7000 -o abcjs_basic_$1-min.js abcjs_all.js
+java -jar yuicompressor-2.4.2.jar  --line-break 7000 -o abcjs_editor_$1-min.js abcjs_editor.js
+java -jar yuicompressor-2.4.2.jar  --line-break 7000 -o abcjs_plugin-nojquery_$1-min.js abcjs_plugin.js
+cat jquery-1.4.2.min.js abcjs_plugin-nojquery_$1-min.js > abcjs_plugin_$1-min.js
 
