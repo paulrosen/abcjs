8571ea9fb33e5d525c635969c3362b172aa6e3e1
Paul Rosen <paul@paulrosen.net>
2014-07-14 10:33:49 -0400

    Put the correct measure number on the beams and ties. Put the correct measure number on the second staff.

--- write/abc_voice_element.js
+++ write/abc_voice_element.js
@@ -33,6 +33,17 @@ ABCJS.write.VoiceElement = function(voicenumber, voicetotal) {
 };
 
 ABCJS.write.VoiceElement.prototype.addChild = function (child) {
+	if (child.type === 'bar') {
+		var firstItem = true;
+		for (var i = 0; firstItem && i < this.children.length; i++) {
+			if (this.children[i].type !== "staff-extra")
+				firstItem = false;
+		}
+		if (!firstItem) {
+			this.beams.push("bar");
+			this.otherchildren.push("bar");
+		}
+	}
 	this.children[this.children.length] = child;
 };
 
@@ -126,6 +137,7 @@ ABCJS.write.VoiceElement.prototype.draw = function (renderer, bartop) {
 	renderer.staffbottom = this.staff.bottom;
 	this.barbottom = renderer.calcY(2);
 
+	renderer.measureNumber = null;
 	if (this.header) { // print voice name
 		var textpitch = 12 - (this.voicenumber+1)*(12/(this.voicetotal+1));
 		var headerX = (this.startx-renderer.paddingleft)/2+renderer.paddingleft;
@@ -144,12 +156,22 @@ ABCJS.write.VoiceElement.prototype.draw = function (renderer, bartop) {
 		if (child.type === 'bar' && !justInitializedMeasureNumber)
 			renderer.measureNumber++;
 	}
+
+	renderer.measureNumber = 0;
 	window.ABCJS.parse.each(this.beams, function(beam) {
-		beam.draw(renderer); // beams must be drawn first for proper printing of triplets, slurs and ties.
+		if (beam === 'bar')
+			renderer.measureNumber++;
+		else
+			beam.draw(renderer); // beams must be drawn first for proper printing of triplets, slurs and ties.
 	});
+
+	renderer.measureNumber = 0;
 	var self = this;
 	window.ABCJS.parse.each(this.otherchildren, function(child) {
-		child.draw(renderer,self.startx+10,width);
+		if (child === 'bar')
+			renderer.measureNumber++;
+		else
+			child.draw(renderer,self.startx+10,width);
 	});
 
 };
