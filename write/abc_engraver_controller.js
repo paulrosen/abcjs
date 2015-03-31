0742aa797c86f7a213fb4b23648d09f79338c059
Paul Rosen <paul@paulrosen.net>
2015-03-31 09:40:25 -0400

    Fix staffwidth multiplier

--- write/abc_engraver_controller.js
+++ write/abc_engraver_controller.js
@@ -54,8 +54,8 @@ ABCJS.write.EngraverController = function(paper, params) {
 	if (params.staffwidth) {
 		// Note: Normally all measurements to the engraver are in POINTS. However, if a person is formatting for the
 		// screen and directly inputting the width, then it is more logical to have the measurement in pixels.
-		this.staffwidthScreen = params.staffwidth * 1.66;
-		this.staffwidthPrint = params.staffwidth * 1.66;
+		this.staffwidthScreen = params.staffwidth;
+		this.staffwidthPrint = params.staffwidth;
 	} else {
 		this.staffwidthScreen = 740; // TODO-PER: Not sure where this number comes from, but this is how it's always been.
 		this.staffwidthPrint = 680; // The number of pixels in 8.5", after 1cm of margin has been removed.
