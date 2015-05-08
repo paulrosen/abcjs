4313963d332f01d4caaed29a3a3f0fcbf1f982f9
Paul Rosen <paul@paulrosen.net>
2015-05-08 02:57:02 -0400

    Add some debugging capability to the plugin.

--- plugin/abc_plugin.js
+++ plugin/abc_plugin.js
@@ -16,17 +16,19 @@
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //    requires: abcjs, raphael, jquery
+/*global jQuery, ABCJS, Raphael, abcjs_is_user_script, abcjs_plugin_autostart */
 
 if (!window.ABCJS)
 	window.ABCJS = {};
 
 window.ABCJS.Plugin = function() {
+	"use strict";
 	var is_user_script = false;
 	try {
 		is_user_script = abcjs_is_user_script;
 	} catch (ex) {
 	}
-  this.show_midi = !is_user_script || this.$.browser.mozilla;	// midi currently only works in Firefox, so in the userscript, don't complicate it.
+  this.show_midi = !is_user_script; // TODO-PER: jQuery isn't yet available, so this part of the test will crash. || $.browser.mozilla;	// midi currently only works in Firefox, so in the userscript, don't complicate it.
   this.hide_abc = false;
   this.render_before = false;
   this.midi_options = {};
@@ -37,38 +39,47 @@ window.ABCJS.Plugin = function() {
   this.auto_render_threshold = 20;
   this.show_text = "show score for: ";
   //this.hide_text = "hide score for: ";
+	this.debug = false;
 };
+window.ABCJS.plugin = new window.ABCJS.Plugin();
 
-window.ABCJS.Plugin.prototype.start = function(jq) {
-	this.$ = jq;
-	var body = jq("body");
+jQuery(function($) {
+	"use strict";
+window.ABCJS.plugin.start = function() {
+	var body = $("body");
   this.errors="";
-  var elems = this.getABCContainingElements(this.$("body"));
+  var elems = this.getABCContainingElements(body);
+	if (this.debug) {
+		for (var i = 0; i < elems.length; i++) {
+			var str = "Possible ABC found (" + (i+1) + " of " + elems.length + "):\n\n" + elems[i].innerText;
+			alert(str);
+		}
+	}
   var self = this;
   var divs = elems.map(function(i,elem){
       return self.convertToDivs(elem);
     });
   this.auto_render = (divs.size()<=this.auto_render_threshold);
   divs.each(function(i,elem){
-      self.render(elem,self.$(elem).data("abctext"));
+      self.render(elem,$(elem).data("abctext"));
     });
 };
 
 // returns a jquery set of the descendants (including self) of elem which have a text node which matches "X:"
-window.ABCJS.Plugin.prototype.getABCContainingElements = function(elem) {
-  var results = this.$();
+window.ABCJS.plugin.getABCContainingElements = function(elem) {
+  var results = $();
   var includeself = false; // whether self is already included (no need to include it again)
   var self = this;
   // TODO maybe look to see whether it's even worth it by using textContent ?
-  this.$(elem).contents().each(function() { 
-      if (this.nodeType == 3 && !includeself) {
+  $(elem).contents().each(function() {
+      if (this.nodeType === 3 && !includeself) {
 	if (this.nodeValue.match(/^\s*X:/m)) {
 		if (this.parentNode.tagName.toLowerCase() !== 'textarea') {
-		  results = results.add(self.$(elem));
+		  results = results.add($(elem));
 		  includeself = true;
 		}
 	}
-      } else if (this.nodeType==1 && !self.$(this).is("textarea")) {
+      } else if (this.nodeType===1 && !$(this).is("textarea")) {
 	results = results.add(self.getABCContainingElements(this));
       }
     });
@@ -79,38 +90,38 @@ window.ABCJS.Plugin.prototype.getABCContainingElements = function(elem) {
 // (and it is not in a subelem)
 // for each abc piece, we surround it with a div, store the abctext in the 
 // div's data("abctext") and return an array 
-window.ABCJS.Plugin.prototype.convertToDivs = function (elem) {
+window.ABCJS.plugin.convertToDivs = function (elem) {
   var self = this;
-  var contents = this.$(elem).contents();
+  var contents = $(elem).contents();
   var abctext = "";
   var abcdiv = null;
   var inabc = false;
   var brcount = 0;
-  var results = this.$();
+  var results = $();
   contents.each(function(i,node){
-      if (node.nodeType==3 && !node.nodeValue.match(/^\s*$/)) {
+      if (node.nodeType===3 && !node.nodeValue.match(/^\s*$/)) {
 	brcount=0;
 	var text = node.nodeValue;
 	if (text.match(/^\s*X:/m)) {
 	  inabc=true;
 	  abctext="";
-	  abcdiv=self.$("<div class='"+self.text_classname+"'></div>");
-	  self.$(node).before(abcdiv);
+	  abcdiv=$("<div class='"+self.text_classname+"'></div>");
+	  $(node).before(abcdiv);
 	  if (self.hide_abc) {
 	    abcdiv.hide();
 	  } 
 	}
 	if (inabc) {
 	  abctext += text.replace(/\n+/,"");
-	  abcdiv.append(self.$(node));
+	  abcdiv.append($(node));
 	} 
-      } else if (inabc && self.$(node).is("br")) {
+      } else if (inabc && $(node).is("br")) {
 	abctext += "\n";
-	abcdiv.append(self.$(node));
+	abcdiv.append($(node));
 	brcount++;
 	  } else if (inabc && node.nodeType === 1) {
 		  abctext += "\n";
-		 	abcdiv.append(self.$(node));
+		 	abcdiv.append($(node));
 		  // just swallow this.
       } else if (inabc) { // second br or whitespace textnode
 	inabc = false;
@@ -128,15 +139,18 @@ window.ABCJS.Plugin.prototype.convertToDivs = function (elem) {
   return results.get();
 };
 
-window.ABCJS.Plugin.prototype.render = function (contextnode, abcstring) {
-  var abcdiv = this.$("<div class='"+this.render_classname+"'></div>");
+window.ABCJS.plugin.render = function (contextnode, abcstring) {
+  var abcdiv = $("<div class='"+this.render_classname+"'></div>");
   if (this.render_before) {
-    this.$(contextnode).before(abcdiv);
+    $(contextnode).before(abcdiv);
   } else {
-    this.$(contextnode).after(abcdiv);
+    $(contextnode).after(abcdiv);
   }
   var self = this;
   try {
+	  if (this.debug) {
+		  alert("About to render:\n\n" + abcstring);
+	  }
     var tunebook = new ABCJS.TuneBook(abcstring);
     var abcParser = new ABCJS.parse.Parse();
     abcParser.parse(tunebook.tunes[0].abc);
@@ -150,14 +164,14 @@ window.ABCJS.Plugin.prototype.render = function (contextnode, abcstring) {
 	} catch (ex) { // f*** internet explorer doesn't like innerHTML in weird situations
 	  // can't remember why we don't do this in the general case, but there was a good reason
 	  abcdiv.remove();
-	  abcdiv = this.$("<div class='"+self.render_classname+"'></div>");
+	  abcdiv = $("<div class='"+self.render_classname+"'></div>");
 	  paper = Raphael(abcdiv.get(0), 800, 400);
 	  engraver_controller = new ABCJS.write.EngraverController(paper);
 	  engraver_controller.engraveABC(tune);
 	  if (self.render_before) {
-	    this.$(contextnode).before(abcdiv);
+	    $(contextnode).before(abcdiv);
 	  } else {
-	    this.$(contextnode).after(abcdiv);
+	    $(contextnode).after(abcdiv);
 	  }
 	}
 	if (ABCJS.MidiWriter && self.show_midi) {
@@ -171,7 +185,7 @@ window.ABCJS.Plugin.prototype.render = function (contextnode, abcstring) {
     if (this.auto_render) {
       doPrint();
     } else {
-      var showspan = this.$(showtext);
+      var showspan = $(showtext);
       showspan.click(function(){
 	  doPrint();
 	  showspan.hide();
@@ -185,11 +199,9 @@ window.ABCJS.Plugin.prototype.render = function (contextnode, abcstring) {
    }
 };
 
-window.ABCJS.plugin = new window.ABCJS.Plugin();
 
 // There may be a variable defined which controls whether to automatically run the script. If it isn't
 // there then it will throw an exception, so we'll catch it here.
-$(document).ready(function() {
 	var autostart = true;
 	try {
 		autostart = abcjs_plugin_autostart;
@@ -197,5 +209,5 @@ $(document).ready(function() {
 		// It's ok to fail, and we don't have to do anything.
 	}
 	if (autostart)
-		jQuery(document).ready(ABCJS.plugin.start(jQuery));
-})();
+		ABCJS.plugin.start();
+});
