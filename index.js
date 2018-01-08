66481b43bf33c9cad484f1b36813ed729322e777
Paul Rosen <paul@paulrosen.net>
2018-01-08 16:54:34 -0500

    Move the source code to a subfolder.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var animation = require('./api/abc_animation');
-var tunebook = require('./api/abc_tunebook');
+var animation = require('./src/api/abc_animation');
+var tunebook = require('./src/api/abc_tunebook');
 
 var abcjs = {};
 
@@ -11,34 +11,34 @@ Object.keys(tunebook).forEach(function (key) {
   abcjs[key] = tunebook[key];
 });
 
-abcjs.renderAbc = require('./api/abc_tunebook_svg');
-abcjs.renderMidi = require('./api/abc_tunebook_midi');
+abcjs.renderAbc = require('./src/api/abc_tunebook_svg');
+abcjs.renderMidi = require('./src/api/abc_tunebook_midi');
 
-var parser = require('./parse/abc_parse');
+var parser = require('./src/parse/abc_parse');
 abcjs['parse'] = { Parse: parser };
 
-var engraverController = require('./write/abc_engraver_controller');
+var engraverController = require('./src/write/abc_engraver_controller');
 abcjs['write'] = { EngraverController: engraverController };
 
 // TODO-PER: Temporary
-var editor = require('./edit/abc_editor');
+var editor = require('./src/edit/abc_editor');
 abcjs['Editor'] = editor;
 
 // TODO-PER: Temporary
-var midi = require('./midi/abc_midi_controls');
-var sequence = require('./midi/abc_midi_sequencer');
-var flatten = require('./midi/abc_midi_flattener');
-var midiCreate = require('./midi/abc_midi_create');
+var midi = require('./src/midi/abc_midi_controls');
+var sequence = require('./src/midi/abc_midi_sequencer');
+var flatten = require('./src/midi/abc_midi_flattener');
+var midiCreate = require('./src/midi/abc_midi_create');
 abcjs['midi'] = midi;
 abcjs['midi'].sequence = sequence;
 abcjs['midi'].flatten = flatten;
 abcjs['midi'].create = midiCreate;
 
 // TODO-PER: Temporary
-var parserLint = require('./test/abc_parser_lint');
-var verticalLint = require('./test/abc_vertical_lint');
-var midiLint = require('./test/abc_midi_lint');
-var midiSequencerLint = require('./test/abc_midi_sequencer_lint');
+var parserLint = require('./src/test/abc_parser_lint');
+var verticalLint = require('./src/test/abc_vertical_lint');
+var midiLint = require('./src/test/abc_midi_lint');
+var midiSequencerLint = require('./src/test/abc_midi_sequencer_lint');
 abcjs['test'] = { ParserLint: parserLint, verticalLint: verticalLint, midiLint: midiLint, midiSequencerLint: midiSequencerLint };
 
 module.exports = abcjs;
