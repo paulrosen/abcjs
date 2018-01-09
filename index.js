e96a95130c339cc18a76fcac97af255f347f3b7f
Paul Rosen <paul@paulrosen.net>
2018-01-09 05:54:13 -0500

    Create separate entry points for midi and the linter files.

--- index.js
+++ index.js
@@ -20,25 +20,7 @@ abcjs['parse'] = { Parse: parser };
 var engraverController = require('./src/write/abc_engraver_controller');
 abcjs['write'] = { EngraverController: engraverController };
 
-// TODO-PER: Temporary
 var editor = require('./src/edit/abc_editor');
 abcjs['Editor'] = editor;
-
-// TODO-PER: Temporary
-var midi = require('./src/midi/abc_midi_controls');
-var sequence = require('./src/midi/abc_midi_sequencer');
-var flatten = require('./src/midi/abc_midi_flattener');
-var midiCreate = require('./src/midi/abc_midi_create');
-abcjs['midi'] = midi;
-abcjs['midi'].sequence = sequence;
-abcjs['midi'].flatten = flatten;
-abcjs['midi'].create = midiCreate;
-
-// TODO-PER: Temporary
-var parserLint = require('./src/test/abc_parser_lint');
-var verticalLint = require('./src/test/abc_vertical_lint');
-var midiLint = require('./src/test/abc_midi_lint');
-var midiSequencerLint = require('./src/test/abc_midi_sequencer_lint');
-abcjs['test'] = { ParserLint: parserLint, verticalLint: verticalLint, midiLint: midiLint, midiSequencerLint: midiSequencerLint };
 
 module.exports = abcjs;
