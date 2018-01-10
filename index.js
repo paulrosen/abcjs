15bd52049ee9e19bf3c15c5b0119f1ed27f60073
Paul Rosen <paul@paulrosen.net>
2018-01-10 04:49:47 -0500

    Remove dependency on midi for the standalone version.

--- index.js
+++ index.js
@@ -4,21 +4,20 @@ var tunebook = require('./src/api/abc_tunebook');
 var abcjs = {};
 
 Object.keys(animation).forEach(function (key) {
-  abcjs[key] = animation[key];
+	abcjs[key] = animation[key];
 });
 
 Object.keys(tunebook).forEach(function (key) {
-  abcjs[key] = tunebook[key];
+	abcjs[key] = tunebook[key];
 });
 
 abcjs.renderAbc = require('./src/api/abc_tunebook_svg');
-abcjs.renderMidi = require('./src/api/abc_tunebook_midi');
 
 var parser = require('./src/parse/abc_parse');
-abcjs['parse'] = { Parse: parser };
+abcjs['parse'] = {Parse: parser};
 
 var engraverController = require('./src/write/abc_engraver_controller');
-abcjs['write'] = { EngraverController: engraverController };
+abcjs['write'] = {EngraverController: engraverController};
 
 var editor = require('./src/edit/abc_editor');
 abcjs['Editor'] = editor;
