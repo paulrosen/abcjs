4d458224b1832e4afe446df742abb1cbb9bb4c72
Paul Rosen <paul@paulrosen.net>
2018-01-11 10:07:48 -0500

    Add polyfill for Object.remove() for IE

--- src/parse/abc_common.js
+++ src/parse/abc_common.js
@@ -93,4 +93,23 @@ parseCommon.detect = function(arr, iterator) {
 	return false;
 };
 
+// The following is a polyfill for Object.remove for IE9, IE10, and IE11.
+// from:https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
+(function (arr) {
+	arr.forEach(function (item) {
+		if (item.hasOwnProperty('remove')) {
+			return;
+		}
+		Object.defineProperty(item, 'remove', {
+			configurable: true,
+			enumerable: true,
+			writable: true,
+			value: function remove() {
+				if (this.parentNode !== null)
+					this.parentNode.removeChild(this);
+			}
+		});
+	});
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+
 module.exports = parseCommon;
