ddb411c3c01b19221ce2382d0b9b9092340ad94b
Paul Rosen <paul@paulrosen.net>
2019-01-28 08:30:39 -0500

    Restrict measure splitter to the first voice for now.

--- src/api/abc_tunebook.js
+++ src/api/abc_tunebook.js
@@ -152,6 +152,73 @@ var tunebook = {};
 		return ret;
 	};
 
+	function flattenTune(tuneObj) {
+		// This removes the line breaks and removes the non-music lines.
+		var staves = [];
+		for (var j = 0; j < tuneObj.lines.length; j++) {
+			var line = tuneObj.lines[j];
+			if (line.staff) {
+				for (var k = 0; k < line.staff.length; k++) {
+					var staff = line.staff[k];
+					if (!staves[k])
+						staves[k] = staff;
+					else {
+						for (var i = 0; i < staff.voices.length; i++) {
+							if (staves[k].voices[i])
+								staves[k].voices[i] = staves[k].voices[i].concat(staff.voices[i]);
+							// TODO-PER: If staves[k].voices[i] doesn't exist, that means a voice appeared in the middle of the tune. That isn't handled yet.
+						}
+					}
+				}
+			}
+		}
+		return staves;
+	}
+
+	function measuresParser(staff, tune) {
+		var voices = [];
+		var lastChord = null;
+		var measureStartChord = null;
+		var fragStart = null;
+		var hasNotes = false;
+
+		for (var i = 0; i < staff.voices.length; i++) {
+			var voice = staff.voices[i];
+			voices.push([]);
+			for (var j = 0; j < voice.length; j++) {
+				var elem = voice[j];
+				if (fragStart === null && elem.startChar >= 0) {
+					fragStart = elem.startChar;
+					if (elem.chord === undefined)
+						measureStartChord = lastChord;
+					else
+						measureStartChord = null;
+				}
+				if (elem.chord)
+					lastChord = elem;
+				if (elem.el_type === 'bar') {
+					if (hasNotes) {
+						var frag = tune.abc.substring(fragStart, elem.endChar);
+						var measure = {abc: frag};
+						lastChord = measureStartChord && measureStartChord.chord && measureStartChord.chord.length > 0 ? measureStartChord.chord[0].name : null;
+						if (lastChord)
+							measure.lastChord = lastChord;
+						if (elem.startEnding)
+							measure.startEnding = elem.startEnding;
+						if (elem.endEnding)
+							measure.endEnding = elem.endEnding;
+						voices[i].push(measure);
+						fragStart = null;
+						hasNotes = false;
+					}
+				} else if (elem.el_type === 'note') {
+					hasNotes = true;
+				}
+			}
+		}
+		return voices;
+	}
+
 	tunebook.extractMeasures = function(abc) {
 		var tunes = [];
 		var book = new TuneBook(abc);
@@ -167,12 +234,35 @@ var tunebook = {};
 			var hasNotes = false;
 			var tuneObj = tunebook.parseOnly(tune.abc)[0];
 			var hasPickup = tuneObj.getPickupLength() > 0;
+			// var staves = flattenTune(tuneObj);
+			// for (var s = 0; s < staves.length; s++) {
+			// 	var voices = measuresParser(staves[s], tune);
+			// 	if (s === 0)
+			// 		measures = voices;
+			// 	else {
+			// 		for (var ss = 0; ss < voices.length; ss++) {
+			// 			var voice = voices[ss];
+			// 			if (measures.length <= ss)
+			// 				measures.push([]);
+			// 			var measureVoice = measures[ss];
+			// 			for (var sss = 0; sss < voice.length; sss++) {
+			// 				if (measureVoice.length > sss)
+			// 					measureVoice[sss].abc += "\n" + voice[sss].abc;
+			// 				else
+			// 					measures.push(voice[sss]);
+			// 			}
+			// 		}
+			// 	}
+			// 	console.log(voices);
+			// }
+			// measures = measures[0];
+
 			for (var j = 0; j < tuneObj.lines.length; j++) {
 				var line = tuneObj.lines[j];
 				if (line.staff) {
-					for (var k = 0; k < line.staff.length; k++) {
+					for (var k = 0; k < 1 /*line.staff.length*/; k++) {
 						var staff = line.staff[k];
-						for (var kk = 0; kk < staff.voices.length; kk++) {
+						for (var kk = 0; kk < 1 /*staff.voices.length*/; kk++) {
 							var voice = staff.voices[kk];
 							for (var kkk = 0; kkk < voice.length; kkk++) {
 								var elem = voice[kkk];
