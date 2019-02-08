45b309d28e1354b7c244a5575bca20c729714bd7
Paul Rosen <paul@paulrosen.net>
2019-02-08 11:56:35 -0500

    Add random access to the timing routine; pass back more progress info in the beat callback.

--- src/api/abc_timing_callbacks.js
+++ src/api/abc_timing_callbacks.js
@@ -31,6 +31,7 @@ var TimingCallbacks = function(target, params) {
 	self.noteTimings = target.noteTimings;
 	self.millisecondsPerBeat = 1000 / (self.qpm / 60);
 	self.lastMoment = self.noteTimings[self.noteTimings.length-1].milliseconds;
+	self.totalBeats = Math.floor(self.lastMoment / self.millisecondsPerBeat);
 
 	self.startTime = null;
 	self.currentBeat = 0;
@@ -38,6 +39,38 @@ var TimingCallbacks = function(target, params) {
 	self.isPaused = false;
 	self.pausedTime = null;
 	self.justUnpaused = false;
+
+	self.newSeekPercent = 0;
+	self.justSeeked = false;
+
+	function setCurrentLocation(timestamp) {
+		// First find the relative amount to move: that is, the difference between the current percentage and the passed in percent.
+		var currentPercent = (timestamp - self.startTime) / self.lastMoment;
+		var percentDifference = currentPercent - self.newSeekPercent;
+		var timeDifference = self.lastMoment * percentDifference;
+		self.startTime = self.startTime + timeDifference;
+
+		var currentTime = timestamp - self.startTime;
+		currentTime += 50; // Add a little slop because this function isn't called exactly.
+
+		self.currentBeat = Math.floor(currentTime/self.millisecondsPerBeat);
+			if (self.beatCallback)
+				self.beatCallback(self.currentBeat, self.totalBeats, self.lastMoment);
+
+		self.currentEvent = 0;
+		while (self.noteTimings.length > self.currentEvent && self.noteTimings[self.currentEvent].milliseconds < currentTime) {
+			self.currentEvent++;
+		}
+		if (self.eventCallback && self.currentEvent > 0 &&  self.noteTimings[self.currentEvent-1].type === 'event')
+			self.eventCallback(self.noteTimings[self.currentEvent-1]);
+
+		// console.log("currentPercent="+currentPercent+
+		// 	" newSeekPercent="+self.newSeekPercent+
+		// 	" percentDifference="+percentDifference+
+		// 	" timeDifference=",timeDifference+
+		// 	" currentBeat="+self.currentBeat+
+		// 	" currentEvent="+self.currentEvent);
+	}
 
 	self.doTiming = function(timestamp) {
 		if (!self.startTime) {
@@ -52,6 +85,10 @@ var TimingCallbacks = function(target, params) {
 		if (self.isPaused) {
 			self.pausedTime = timestamp;
 		} else {
+			if (self.justSeeked) {
+				setCurrentLocation(timestamp);
+				self.justSeeked = false;
+			}
 			var currentTime = timestamp - self.startTime;
 			currentTime += 50; // Add a little slop because this function isn't called exactly.
 			if (currentTime < self.lastMoment)
@@ -59,7 +96,7 @@ var TimingCallbacks = function(target, params) {
 
 			if (self.currentBeat * self.millisecondsPerBeat < currentTime) {
 				if (self.beatCallback)
-					self.beatCallback(self.currentBeat);
+					self.beatCallback(self.currentBeat, self.totalBeats, self.lastMoment);
 				self.currentBeat++;
 			}
 
@@ -101,6 +138,15 @@ var TimingCallbacks = function(target, params) {
 	self.stop = function() {
 		self.pause();
 		self.reset();
+	};
+	self.setProgress = function(percent) {
+		// this is passed a value between 0 and 1.
+		// the effect of this function is to move startTime so that the callbacks happen correctly for the new seek.
+		if (percent < 0) percent = 0;
+		if (percent > 1) percent = 1;
+
+		self.newSeekPercent = percent;
+		self.justSeeked = true;
 	};
 };
 
