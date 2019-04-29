9bfe216f438c4ee864c8ff0e7e5a4b3cba38370d
堀江良 <develop@ryohorie.com>
2019-04-29 12:19:12 +0900

    add generator

---
+++ rehearsalmark_generator/index.js
@@ -0,0 +1,36 @@
+const TextToSVG = require('text-to-svg');
+var svgpath = require('svgpath');
+
+const textToSVG = TextToSVG.loadSync('./NotoSerifCJKjp-Bold.otf');
+
+const attributes = {fill: 'black', stroke: 'black'};
+const options = {x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes};
+
+
+const dash = svgpath(textToSVG.getD("’", options)).rel().scale(0.75).translate(50,10)
+const dash2 = svgpath(textToSVG.getD("’", options)).rel().scale(0.75).translate(60,10)
+
+for(var i = 0; i < 26; i++){
+  const text = String.fromCodePoint('🄰'.codePointAt(0)+i)
+  const d = textToSVG.getD(text, options);
+  const char = String.fromCharCode('A'.charCodeAt(0)+i)
+  const data = {}
+  data[`[${char}]`] = d
+  data[`[${char}\\']`] = d + dash
+  data[`[${char}\\'\\']`] = d + dash + dash2
+  for(index in data) {
+    var transformed = svgpath(data[index])
+                      .scale(0.33)
+                      .translate(-15,-35)
+                      .rel()
+                      .round(3)
+    const json = JSON.stringify(
+      {
+        d:transformed.segments,
+        w:30,
+				h:0
+      }
+    )
+    console.log(`'scripts.${index}':${json},`)
+  }
+}
