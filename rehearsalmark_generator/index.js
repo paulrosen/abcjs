c7462a03287358df79391a50485420b5de97e555
堀江良 <develop@ryohorie.com>
2019-05-02 23:44:16 +0900

    Intro, Endingを追加

--- rehearsalmark_generator/index.js
+++ rehearsalmark_generator/index.js
@@ -9,28 +9,53 @@ const options = {x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes
 
 const dash = svgpath(textToSVG.getD("’", options)).rel().scale(0.75).translate(50,10)
 const dash2 = svgpath(textToSVG.getD("’", options)).rel().scale(0.75).translate(60,10)
+const data = {}
 
 for(var i = 0; i < 26; i++){
   const text = String.fromCodePoint('🄰'.codePointAt(0)+i)
   const d = textToSVG.getD(text, options);
   const char = String.fromCharCode('A'.charCodeAt(0)+i)
-  const data = {}
-  data[`${char}`] = d
-  data[`${char}\\'`] = d + dash
-  data[`${char}\\'\\'`] = d + dash + dash2
-  for(index in data) {
-    var transformed = svgpath(data[index])
-                      .scale(0.33)
-                      .translate(-33,-85)
-                      .rel()
-                      .round(3)
-    const json = JSON.stringify(
-      {
-        d:transformed.segments,
-        w:0,
-				h:0
-      }
-    )
-    console.log(`'scripts.${index}':${json},`)
+  const tmp = {}
+  tmp[`${char}`] = {}
+  tmp[`${char}\\'`] = {}
+  tmp[`${char}\\'\\'`] = {}
+  tmp[`${char}`].svg = d
+  tmp[`${char}\\'`].svg = d + dash
+  tmp[`${char}\\'\\'`].svg = d + dash + dash2
+  for(j in tmp) {
+    tmp[j].scaleX = 0.33
+    tmp[j].scaleY = 0.33
+    tmp[j].translateX = -33
+    tmp[j].translateY = -85
+    data[j] = tmp[j]
   }
+}
+data['Intro'] = {}
+data['Intro'].svg = textToSVG.getD('Intro', options)
+data['Intro'].scaleX = 0.15
+data['Intro'].scaleY = 0.23
+data['Intro'].translateX = -33
+data['Intro'].translateY = -85
+
+data['Ending'] = {}
+data['Ending'].svg = textToSVG.getD('Ending', options)
+data['Ending'].scaleX = 0.13
+data['Ending'].scaleY = 0.23
+data['Ending'].translateX = -33
+data['Ending'].translateY = -85
+
+for(index in data) {
+  var transformed = svgpath(data[index].svg)
+                    .scale(data[index].scaleX, data[index].scaleY)
+                    .translate(data[index].translateX, data[index].translateY)
+                    .rel()
+                    .round(3)
+  const json = JSON.stringify(
+    {
+      d:transformed.segments,
+      w:0,
+      h:0
+    }
+  )
+  console.log(`'scripts.${index}':${json},`)
 }
