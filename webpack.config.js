1bd48e4262ed05815c4535192237b8249c49958a
Paul Rosen <paul@paulrosen.net>
2019-07-08 23:26:22 -0400

    Update webpack/babel to the latest versions.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const version = pJson.version;
 
 module.exports = (env) => {
 	const outputFilename = `abcjs_${env.type}_${version}-min.js`;
-	const banner1 = `abcjs_${env.type} v${version} Copyright © 2009-2018 Paul Rosen and Gregory Dyke (http://abcjs.net) */`;
+	const banner1 = `abcjs_${env.type} v${version} Copyright © 2009-2019 Paul Rosen and Gregory Dyke (http://abcjs.net) */`;
 	const banner2 = `/*! midi.js Copyright © Michael Deal (http://mudcu.be) */`;
 	const banner3 = `/*! For license information please see ${outputFilename}.LICENSE`;
 	const hasMidi = env.type === 'midi';
