2f28422511eb29eec6ce921bf756284695563f3a
Paul Rosen <paul@paulrosen.net>
2020-03-01 10:09:43 -0500

    Make the rendered music fit the width of the vuepress content.

--- docs/.vuepress/components/RenderAbc.vue
+++ docs/.vuepress/components/RenderAbc.vue
@@ -17,13 +17,14 @@
 		data() {
 			return {
 				visualObj: null,
+				defaultOptions: { paddingleft: 0, paddingright: 0, responsive: "resize" },
 			};
 		},
 		mounted() {
 			Vue.nextTick(() => {
 				const abcjs = require('abcjs');
 				const el = this.$refs.paper;
-				this.visualObj = abcjs.renderAbc(el, this.abc);
+				this.visualObj = abcjs.renderAbc(el, this.abc, this.defaultOptions);
 			});
 		},
 		methods: {
