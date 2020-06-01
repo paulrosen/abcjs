9f6221055d61d7cb52b03806b50364c9d92bbbe7
Paul Rosen <paul@paulrosen.net>
2020-05-31 21:09:59 -0400

    Refactor some of the layout calculations to a separate file.

--- src/write/abc_engraver_controller.js
+++ src/write/abc_engraver_controller.js
@@ -21,6 +21,7 @@ var spacing = require('./abc_spacing');
 var AbstractEngraver = require('./abc_abstract_engraver');
 var Renderer = require('./abc_renderer');
 var setupSelection = require('./selection');
+var layout = require('./layout');
 
 /**
  * @class
@@ -207,33 +208,7 @@ EngraverController.prototype.engraveTune = function (abctune, tuneNumber) {
 	}
 
 	// Adjust the x-coordinates to their absolute positions
-	var maxWidth = this.width;
-	for(i=0; i<abctune.lines.length; i++) {
-		abcLine = abctune.lines[i];
-		if (abcLine.staff) {
-			this.setXSpacing(abcLine.staffGroup, abctune.formatting, i === abctune.lines.length - 1, false);
-			if (abcLine.staffGroup.w > maxWidth) maxWidth = abcLine.staffGroup.w;
-		}
-	}
-
-	// Layout the beams and add the stems to the beamed notes.
-	for(i=0; i<abctune.lines.length; i++) {
-		abcLine = abctune.lines[i];
-		if (abcLine.staffGroup && abcLine.staffGroup.voices) {
-			for (var j = 0; j < abcLine.staffGroup.voices.length; j++)
-				abcLine.staffGroup.voices[j].layoutBeams();
-			abcLine.staffGroup.setUpperAndLowerElements(this.renderer);
-		}
-	}
-
-	// Set the staff spacing
-	// TODO-PER: we should have been able to do this by the time we called setUpperAndLowerElements, but for some reason the "bottom" element seems to be set as a side effect of setting the X spacing.
-	for(i=0; i<abctune.lines.length; i++) {
-		abcLine = abctune.lines[i];
-		if (abcLine.staffGroup) {
-			abcLine.staffGroup.height = abcLine.staffGroup.calcHeight();
-		}
-	}
+	var maxWidth = layout(this.renderer, abctune, this.width, this.space);
 
 	// Do all the writing to output
 	this.renderer.topMargin(abctune);
@@ -305,43 +280,6 @@ EngraverController.prototype.combineHistory = function (len, svgEl) {
 	this.history.push(items[0]);
 };
 
-function calcHorizontalSpacing(isLastLine, stretchLast, targetWidth, lineWidth, spacing, spacingUnits, minSpace) {
-	// TODO-PER: This used to stretch the first line when it is the only line, but I'm not sure why. abcm2ps doesn't do that
-	if (isLastLine && lineWidth / targetWidth < 0.66 && !stretchLast) return null; // don't stretch last line too much
-	if (Math.abs(targetWidth-lineWidth) < 2) return null; // if we are already near the target width, we're done.
-	var relSpace = spacingUnits * spacing;
-	var constSpace = lineWidth - relSpace;
-	if (spacingUnits > 0) {
-		spacing = (targetWidth - constSpace) / spacingUnits;
-		if (spacing * minSpace > 50) {
-			spacing = 50 / minSpace;
-		}
-		return spacing;
-	}
-	return null;
-}
-
-/**
- * Do the x-axis positioning for a single line (a group of related staffs)
- * @param {ABCJS.Tune} abctune an ABCJS AST
- * @param {Object} staffGroup an staffGroup
- * @param {Object} formatting an formatting
- * @param {boolean} isLastLine is this the last line to be printed?
- * @private
- */
-EngraverController.prototype.setXSpacing = function (staffGroup, formatting, isLastLine, debug) {
-   var newspace = this.space;
-  for (var it = 0; it < 8; it++) { // TODO-PER: shouldn't need multiple passes, but each pass gets it closer to the right spacing. (Only affects long lines: normal lines break out of this loop quickly.)
-	  var ret = staffGroup.layout(newspace, this.renderer, debug);
-	  var stretchLast = formatting.stretchlast ? formatting.stretchlast : false;
-		newspace = calcHorizontalSpacing(isLastLine, stretchLast, this.width+this.renderer.padding.left, staffGroup.w, newspace, ret.spacingUnits, ret.minSpace);
-		if (debug)
-			console.log("setXSpace", it, staffGroup.w, newspace, staffGroup.minspace);
-		if (newspace === null) break;
-  }
-	centerWholeRests(staffGroup.voices);
-	//this.renderer.printHorizontalLine(this.width);
-};
 
 /**
  * Engrave a single line (a group of related staffs)
@@ -364,25 +302,5 @@ EngraverController.prototype.engraveStaffLine = function (staffGroup) {
 EngraverController.prototype.addSelectListener = function (clickListener) {
 	this.listeners[this.listeners.length] = clickListener;
 };
-
-function centerWholeRests(voices) {
-	// whole rests are a special case: if they are by themselves in a measure, then they should be centered.
-	// (If they are not by themselves, that is probably a user error, but we'll just center it between the two items to either side of it.)
-	for (var i = 0; i < voices.length; i++) {
-		var voice = voices[i];
-		// Look through all of the elements except for the first and last. If the whole note appears there then there isn't anything to center it between anyway.
-		for (var j = 1; j < voice.children.length-1; j++) {
-			var absElem = voice.children[j];
-			if (absElem.abcelem.rest && (absElem.abcelem.rest.type === 'whole' || absElem.abcelem.rest.type === 'multimeasure')) {
-				var before = voice.children[j-1];
-				var after = voice.children[j+1];
-				var midpoint = (after.x - before.x) / 2 + before.x;
-				absElem.x = midpoint - absElem.w / 2;
-				for (var k = 0; k < absElem.children.length; k++)
-					absElem.children[k].x = absElem.x;
-			}
-		}
-	}
-}
 
 module.exports = EngraverController;
---
+++ src/write/layout.js
@@ -0,0 +1,90 @@
+var layout;
+
+(function () {
+	"use strict";
+	layout = function (renderer, abctune, width, space) {
+		var i;
+		var abcLine;
+		// Adjust the x-coordinates to their absolute positions
+		var maxWidth = width;
+		for(i=0; i<abctune.lines.length; i++) {
+			abcLine = abctune.lines[i];
+			if (abcLine.staff) {
+				setXSpacing(renderer, width, space, abcLine.staffGroup, abctune.formatting, i === abctune.lines.length - 1, false);
+				if (abcLine.staffGroup.w > maxWidth) maxWidth = abcLine.staffGroup.w;
+			}
+		}
+
+		// Layout the beams and add the stems to the beamed notes.
+		for(i=0; i<abctune.lines.length; i++) {
+			abcLine = abctune.lines[i];
+			if (abcLine.staffGroup && abcLine.staffGroup.voices) {
+				for (var j = 0; j < abcLine.staffGroup.voices.length; j++)
+					abcLine.staffGroup.voices[j].layoutBeams();
+				abcLine.staffGroup.setUpperAndLowerElements(renderer);
+			}
+		}
+
+		// Set the staff spacing
+		// TODO-PER: we should have been able to do this by the time we called setUpperAndLowerElements, but for some reason the "bottom" element seems to be set as a side effect of setting the X spacing.
+		for(i=0; i<abctune.lines.length; i++) {
+			abcLine = abctune.lines[i];
+			if (abcLine.staffGroup) {
+				abcLine.staffGroup.height = abcLine.staffGroup.calcHeight();
+			}
+		}
+		return maxWidth;
+	}
+	// Do the x-axis positioning for a single line (a group of related staffs)
+	var setXSpacing = function (renderer, width, space, staffGroup, formatting, isLastLine, debug) {
+		var newspace = space;
+		for (var it = 0; it < 8; it++) { // TODO-PER: shouldn't need multiple passes, but each pass gets it closer to the right spacing. (Only affects long lines: normal lines break out of this loop quickly.)
+			var ret = staffGroup.layout(newspace, renderer, debug);
+			var stretchLast = formatting.stretchlast ? formatting.stretchlast : false;
+			newspace = calcHorizontalSpacing(isLastLine, stretchLast, width+renderer.padding.left, staffGroup.w, newspace, ret.spacingUnits, ret.minSpace);
+			if (debug)
+				console.log("setXSpace", it, staffGroup.w, newspace, staffGroup.minspace);
+			if (newspace === null) break;
+		}
+		centerWholeRests(staffGroup.voices);
+		//renderer.printHorizontalLine(width);
+	};
+
+	function calcHorizontalSpacing(isLastLine, stretchLast, targetWidth, lineWidth, spacing, spacingUnits, minSpace) {
+		// TODO-PER: This used to stretch the first line when it is the only line, but I'm not sure why. abcm2ps doesn't do that
+		if (isLastLine && lineWidth / targetWidth < 0.66 && !stretchLast) return null; // don't stretch last line too much
+		if (Math.abs(targetWidth-lineWidth) < 2) return null; // if we are already near the target width, we're done.
+		var relSpace = spacingUnits * spacing;
+		var constSpace = lineWidth - relSpace;
+		if (spacingUnits > 0) {
+			spacing = (targetWidth - constSpace) / spacingUnits;
+			if (spacing * minSpace > 50) {
+				spacing = 50 / minSpace;
+			}
+			return spacing;
+		}
+		return null;
+	}
+
+	function centerWholeRests(voices) {
+		// whole rests are a special case: if they are by themselves in a measure, then they should be centered.
+		// (If they are not by themselves, that is probably a user error, but we'll just center it between the two items to either side of it.)
+		for (var i = 0; i < voices.length; i++) {
+			var voice = voices[i];
+			// Look through all of the elements except for the first and last. If the whole note appears there then there isn't anything to center it between anyway.
+			for (var j = 1; j < voice.children.length-1; j++) {
+				var absElem = voice.children[j];
+				if (absElem.abcelem.rest && (absElem.abcelem.rest.type === 'whole' || absElem.abcelem.rest.type === 'multimeasure')) {
+					var before = voice.children[j-1];
+					var after = voice.children[j+1];
+					var midpoint = (after.x - before.x) / 2 + before.x;
+					absElem.x = midpoint - absElem.w / 2;
+					for (var k = 0; k < absElem.children.length; k++)
+						absElem.children[k].x = absElem.x;
+				}
+			}
+		}
+	}
+})();
+
+module.exports = layout;
