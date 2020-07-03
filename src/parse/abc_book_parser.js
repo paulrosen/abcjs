6d9595001a73912f3a8dc274d9127fdabee3ea92
8633brown <toddbrown.8633@yahoo.co.uk>
2020-07-03 16:22:53 -0600

    use ES6 const and export default

--- src/parse/abc_book_parser.js
+++ src/parse/abc_book_parser.js
@@ -14,7 +14,7 @@
 //    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 //    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-var BookParser = function(book) {
+const BookParser = function(book) {
 
   var directives = "";
   book = book.trim();
@@ -63,4 +63,4 @@ var BookParser = function(book) {
   return tunes;
 };
 
-module.exports = BookParser
+export default BookParser
