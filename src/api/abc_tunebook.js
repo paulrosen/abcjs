96ccb7dfbae52273c7671e85b9224b065b3ccb27
8633brown <toddbrown.8633@yahoo.co.uk>
2020-07-05 00:37:32 -0600

    use default in es5 require from es6 file
    ES5 require must use `{object}.default` to import from an ES6 file which
    exports defualt.   https://ponyfoo.com/articles/es6-modules-in-depth
    gives a good overview of ES6 modules.

--- src/api/abc_tunebook.js
+++ src/api/abc_tunebook.js
@@ -19,7 +19,7 @@
 
 var parseCommon = require('../parse/abc_common');
 var Parse = require('../parse/abc_parse');
-var BookParser = require('../parse/abc_book_parser');
+var BookParser = require('../parse/abc_book_parser').default;
 
 var tunebook = {};
 
