01eb5cdd04856c92d86671e406191c59234ce0db
8633brown <toddbrown.8633@yahoo.co.uk>
2020-07-07 21:18:29 -0600

    better usage of parseCommon.strip

--- src/parse/abc_parse_book.js
+++ src/parse/abc_parse_book.js
@@ -58,12 +58,12 @@ var BookParser = function(book) {
 		var title = tune.pure.split("T:");
 		if (title.length > 1) {
 			title = title[1].split("\n");
-			tune.title = title[0].replace(/^\s+|\s+$/g, '');
+			tune.title = parseCommon.strip(title[0]);
 		}
 
 		// for the user's convenience, parse and store the id separately. The id is between the first X: and the next \n
 		var id = tune.pure.substring(2, tune.pure.indexOf("\n"));
-		tune.id = id.replace(/^\s+|\s+$/g, '');
+		tune.id = parseCommon.strip(id);
 	});
 
 	return {
