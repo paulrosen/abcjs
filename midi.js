2e38377c977c219b60b772e771b741ed320afa41
8633brown <toddbrown.8633@yahoo.co.uk>
2020-07-08 14:04:18 -0600

    remove duplicate code
    midi and test are both almost a direct copy of index. let's import the
    index (basic) library and then add the midi parts instead of managing
    2 separate entry points.

--- midi.js
+++ midi.js
@@ -1,50 +1,9 @@
+var abcjs = require('./index');
 var version = require('./version');
-var animation = require('./src/api/abc_animation');
-var tuneBook = require('./src/api/abc_tunebook');
-
-var abcjs = {};
 
 abcjs.signature = "abcjs-midi v" + version;
 
-Object.keys(animation).forEach(function (key) {
-	abcjs[key] = animation[key];
-});
-
-Object.keys(tuneBook).forEach(function (key) {
-	abcjs[key] = tuneBook[key];
-});
-
-abcjs.renderAbc = require('./src/api/abc_tunebook_svg');
 abcjs.renderMidi = require('./src/api/abc_tunebook_midi');
-abcjs.TimingCallbacks = require('./src/api/abc_timing_callbacks');
-
-var glyphs = require('./src/write/abc_glyphs');
-abcjs.setGlyph = glyphs.setSymbol;
-
-var CreateSynth = require('./src/synth/create-synth');
-var instrumentIndexToName = require('./src/synth/instrument-index-to-name');
-var pitchToNoteName = require('./src/synth/pitch-to-note-name');
-var SynthSequence = require('./src/synth/synth-sequence');
-var CreateSynthControl = require('./src/synth/create-synth-control');
-var registerAudioContext = require('./src/synth/register-audio-context');
-var activeAudioContext = require('./src/synth/active-audio-context');
-var playEvent = require('./src/synth/play-event');
-var SynthController = require('./src/synth/synth-controller');
-
-abcjs.synth = {
-	CreateSynth: CreateSynth,
-	instrumentIndexToName: instrumentIndexToName,
-	pitchToNoteName: pitchToNoteName,
-	SynthController: SynthController,
-	SynthSequence: SynthSequence,
-	CreateSynthControl: CreateSynthControl,
-	registerAudioContext: registerAudioContext,
-	activeAudioContext: activeAudioContext,
-	playEvent: playEvent
-};
-
-abcjs['Editor'] = require('./src/edit/abc_editor');
-abcjs['EditArea'] = require('./src/edit/abc_editarea');
 require("./src/midi/abc_midi_ui_generator");
 
 var midi = require('./src/midi/abc_midi_controls');
--- test.js
+++ test.js
@@ -1,58 +1,14 @@
 var version = require('./version');
-var animation = require('./src/api/abc_animation');
-var tuneBook = require('./src/api/abc_tunebook');
-
-var abcjs = {};
 
 abcjs.signature = "abcjs-test v" + version;
 
-Object.keys(animation).forEach(function (key) {
-	abcjs[key] = animation[key];
-});
-
-Object.keys(tuneBook).forEach(function (key) {
-	abcjs[key] = tuneBook[key];
-});
-
-abcjs.renderAbc = require('./src/api/abc_tunebook_svg');
 abcjs.renderMidi = require('./src/api/abc_tunebook_midi');
-abcjs.TimingCallbacks = require('./src/api/abc_timing_callbacks');
-
-var glyphs = require('./src/write/abc_glyphs');
-abcjs.setGlyph = glyphs.setSymbol;
-
-var CreateSynth = require('./src/synth/create-synth');
-var instrumentIndexToName = require('./src/synth/instrument-index-to-name');
-var pitchToNoteName = require('./src/synth/pitch-to-note-name');
-var SynthSequence = require('./src/synth/synth-sequence');
-var CreateSynthControl = require('./src/synth/create-synth-control');
-var registerAudioContext = require('./src/synth/register-audio-context');
-var activeAudioContext = require('./src/synth/active-audio-context');
-var supportsAudio = require('./src/synth/supports-audio');
-var playEvent = require('./src/synth/play-event');
-var SynthController = require('./src/synth/synth-controller');
-
-abcjs.synth = {
-	CreateSynth: CreateSynth,
-	instrumentIndexToName: instrumentIndexToName,
-	pitchToNoteName: pitchToNoteName,
-	SynthController: SynthController,
-	SynthSequence: SynthSequence,
-	CreateSynthControl: CreateSynthControl,
-	registerAudioContext: registerAudioContext,
-	activeAudioContext: activeAudioContext,
-	supportsAudio: supportsAudio,
-	playEvent: playEvent
-};
 
 var parser = require('./src/parse/abc_parse');
 abcjs['parse'] = { Parse: parser };
 
 var engraverController = require('./src/write/abc_engraver_controller');
 abcjs['write'] = { EngraverController: engraverController };
-
-var editor = require('./src/edit/abc_editor');
-abcjs['Editor'] = editor;
 
 var midi = require('./src/midi/abc_midi_controls');
 var sequence = require('./src/midi/abc_midi_sequencer');
