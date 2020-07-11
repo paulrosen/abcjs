1a09218259b1fc457525ffab4b6adff8e524cdc5
8633brown <toddbrown.8633@yahoo.co.uk>
2020-07-10 18:27:15 -0600

    add entry point tests

---
+++ tests/midi.test.js
@@ -0,0 +1,21 @@
+import { default as midi  } from "../midi"
+
+test("exports an object", () => {
+	expect(typeof midi).toBe('object')
+})
+
+test("has correct signature", () => {
+  expect(midi.signature).toMatch(/^abcjs-midi v/)
+})
+
+test("can access midi", () => {
+	expect(typeof midi.midi).toBe('object')
+	expect(typeof midi.midi.setSoundFont).toBe('function')
+	expect(typeof midi.midi.startPlaying).toBe('function')
+	expect(typeof midi.midi.restartPlaying).toBe('function')
+	expect(typeof midi.midi.stopPlaying).toBe('function')
+	expect(typeof midi.midi.setLoop).toBe('function')
+	expect(typeof midi.midi.deviceSupportsMidi).toBe('function')
+	expect(typeof midi.midi.setRandomProgress).toBe('function')
+	expect(typeof midi.midi.setInteractiveProgressBar).toBe('function')
+})
