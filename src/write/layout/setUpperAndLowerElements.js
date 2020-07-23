2b62561834c18a6763d28b1d86a7566f0eb2025f
8633brown <toddbrown.8633@yahoo.co.uk>
2020-07-23 00:33:46 -0600

    move absolute el setUpperAndLower function

--- src/write/layout/setUpperAndLowerElements.js
+++ src/write/layout/setUpperAndLowerElements.js
@@ -90,12 +90,35 @@ function setUpperAndLowerVoiceElements(positionY, voice) {
 	var abselem;
 	for (i = 0; i < voice.children.length; i++) {
 		abselem = voice.children[i];
-		abselem.setUpperAndLowerElements(positionY);
+		setUpperAndLowerAbsoluteElements(positionY, abselem);
 	}
 	for (i = 0; i < voice.otherchildren.length; i++) {
 		abselem = voice.otherchildren[i];
 		if (typeof abselem !== 'string')
 			abselem.setUpperAndLowerElements(positionY);
+	}
+};
+
+// For each of the relative elements that can't be placed in advance (because their vertical placement depends on everything
+// else on the line), this iterates through them and sets their pitch. By the time this is called, specialYResolved contains a
+// hash with the vertical placement (in pitch units) for each type.
+// TODO-PER: I think this needs to be separated by "above" and "below". How do we know that for dynamics at the point where they are being defined, though? We need a pass through all the relative elements to set "above" and "below".
+function setUpperAndLowerAbsoluteElements(specialYResolved, element) {
+	// specialYResolved contains the actual pitch for each of the classes of elements.
+	for (var i = 0; i < element.children.length; i++) {
+		var child = element.children[i];
+		for (var key in element.specialY) { // for each class of element that needs to be placed vertically
+			if (element.specialY.hasOwnProperty(key)) {
+				if (child[key]) { // If this relative element has defined a height for this class of element
+					child.pitch = specialYResolved[key];
+					if (child.top === undefined) { // TODO-PER: HACK! Not sure this is the right place to do this.
+						child.setUpperAndLowerElements(specialYResolved);
+						element.pushTop(child.top);
+						element.pushBottom(child.bottom);
+					}
+				}
+			}
+		}
 	}
 };
 
