6ea1f9ca0942a2b28852d25e20feb860e59642f8
8633brown <toddbrown.8633@yahoo.co.uk>
2020-07-22 19:09:55 -0600

    remove commented out code

--- src/write/draw/triplet.js
+++ src/write/draw/triplet.js
@@ -5,11 +5,7 @@ var printPath = require('./print-path');
 function drawTriplet(renderer, params, selectables) {
 	var xTextPos;
 	renderer.paper.openGroup({ klass: renderer.controller.classes.generate('triplet '+params.durationClass)});
-	if (params.hasBeam) {
-		// var left = params.anchor1.parent.beam.isAbove() ? params.anchor1.x + params.anchor1.w : params.anchor1.x;
-		// xTextPos = params.anchor1.parent.beam.xAtMidpoint(left, params.anchor2.x);
-	} else {
-		// xTextPos = params.anchor1.x + (params.anchor2.x + params.anchor2.w - params.anchor1.x) / 2;
+	if (!params.hasBeam) {
 		drawBracket(renderer, params.anchor1.x, params.startNote, params.anchor2.x + params.anchor2.w, params.endNote);
 	}
 	renderText(renderer, {x: params.xTextPos, y: renderer.calcY(params.yTextPos), text: "" + params.number, type: 'tripletfont', anchor: "middle", centerVertically: true, noClass: true});
