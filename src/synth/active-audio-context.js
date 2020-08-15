d1c5636647c337c770b1c2bfabf116eb160d495b
Paul Rosen <paul@paulrosen.net>
2020-08-15 13:41:13 -0400

    Create AudioContext if one isn't passed in.

--- src/synth/active-audio-context.js
+++ src/synth/active-audio-context.js
@@ -12,8 +12,11 @@
 //    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
 //    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 //    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+var registerAudioContext = require('./register-audio-context.js');
 
 function activeAudioContext() {
+	if (!window.abcjsAudioContext)
+		registerAudioContext();
 	return window.abcjsAudioContext;
 }
 
