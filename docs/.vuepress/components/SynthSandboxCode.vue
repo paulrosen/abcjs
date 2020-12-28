1c4b5277e27b267f589b15c603090d61f5f24e7a
Cameron Baumgartner <cameron.baumgartner@vcfa.edu>
2020-12-27 21:21:18 -0500

    handled sound, changes, timing, and other options

--- docs/.vuepress/components/SynthSandboxCode.vue
+++ docs/.vuepress/components/SynthSandboxCode.vue
@@ -64,55 +64,53 @@ export default {
         }
         else audio = '<div id="audio"></div>';
       }
+      const midi = this.midi ? '<div id="midi-download"></div>' : '';
       return `
 ${paper}
 ${warnings}
-${audio}`;
+${audio}
+${midi}`;
     },
+
     sheetMusicJs() {
-      // console.log(this.changes);
+
       // SETUP
       const abcjs = this.usingNode ? 'abcjs' : 'ABCJS';
+      const visualOptions = {};  // will be passed to renderAbc()
+      var visualObj = ['placeholder'];
+      const synthOptions = {};  // will be passed to mySynth.init
+      const audioParams = {};   // will be passed to synthControl.setTune()
+      if (this.soundfont) {
+        synthOptions.soundFontURL = 'URL';
+        audioParams.soundFontURL = 'URL';
+      }
 
 
-      // TODO: hide measures, cursor that highlights current note
       // VISUAL
-      let visualOptions = {};
       const target = this.sheetMusic ? 'paper' : '*';
       let animation = '';
+      const animationOptions = {};    // will be passed to startAnimation()
       if (this.cursor || this.hideMeasures) {
-        console.log(this.hideMeasures);
-        const params = {};
         if (this.cursor) {
-          console.log('hi from cursor highlight');
-          // add to params
-          params.hi = "cursor highlight";
+          animationOptions.showCursor = true;
         }
-        // TODO: this.hideMeasures = undefined
         if (this.hideMeasures) {
-          console.log('hi from hide measures');
-          // add to params
-          params.hello = 'hide measures';
+          animationOptions.hideFinishedMeasures = true;
         }
-        animation = 
-`// use a button and ABCJS.pauseAnimation() to allow user to pause the animation 
-ABCJS.startAnimation(paper, abc_editor.tunes[0], ${JSON.stringify(params)});`
+        // TODO: startAnimation() is deprecated and should be replaced with an implementation of the TimingCallbacks object.
+
+        animation = `
+ABCJS.startAnimation(paper, abc_editor.tunes[0], ${JSON.stringify(animationOptions)});`
       }
-//       const cursorHighlight = this.cursor ? 
-// ``
-//       const hideMeasures = this.hideMeasures ? 
-// `// use a button and ABCJS.pauseAnimation() to allow user to pause the animation 
-// ABCJS.startAnimation(paper, abc_editor.tunes[0], params);` :
-//       '';
 
 
       // TODO
       // CHANGES
-      let synthOptions = {};  // will be passed to mySynth.init
       let changes = '';
       switch (this.changes) {
         case 'editor':
-          changes = `var abc_editor = new window.ABCJS.Editor("abc", {
+          changes = `
+var abc_editor = new window.ABCJS.Editor("abc", {
   paper_id: "paper",
   warnings_id:"warnings",
   abcjsParams: {
@@ -121,31 +119,29 @@ ABCJS.startAnimation(paper, abc_editor.tunes[0], ${JSON.stringify(params)});`
 });`;
           break;
         case 'drag':
-          // use visualOptions
+          changes = `
+visualOptions.dragging = true;
+visualOptions.clickListener = function(abcelem, tuneNumber, classes, analysis, drag, mouseEvent) { 
+  // modify the ABC string and rerender 
+}`;
           break;
         case 'params':
-          // add to synthOptions
+          // TODO
           break;
         case 'programmatic':
-          // see flow chart
+          // TODO
           break;
       }
 
 
-      // CURSOR
-      const cursorControl = this.usingCallbacks ? 
-`// see docs for parameters
-var cursorControl = new synth.CursorControl();` : 
-'var cursorControl = null;';
-
-
-      // AUDIO CONTROL. Edit out the midiTranspose stuff
-      const widget = this.playbackWidget ? `var mySynth = new ABCJS.synth.CreateSynth();
+      // AUDIO CONTROL
+      const widget = this.playbackWidget ? 
+`var mySynth = new ABCJS.synth.CreateSynth();
 var synthControl = new synth.SynthController();
 var mySynth = new ABCJS.synth.CreateSynth();
 
 mySynth.init(synthOptions).then(function() {
-    synthControl.setTune(visualObj[0], true, { midiTranspose: transposeBy })
+    synthControl.setTune(visualObj[0], true, audioParams)
     .then(function(){
       console.log('Audio successfully loaded.')
     }).catch(function(error) {
@@ -160,30 +156,68 @@ synthControl.load('#audio', cursorControl, {
   displayProgress: ${this.progress},
   displayWarp: ${this.warp},
   displayClock: ${this.clock} 
-});` : `var mySynth = new ABCJS.synth.CreateSynth();
+});` : 
+`var mySynth = new ABCJS.synth.CreateSynth();
 mySynth.init(synthOptions).then(() => { 
-  synth.prime(() => { 
-    ... });
-  .start();`; 
+    synth.prime().then(() => {
+      start();
+    });
+  });`; 
 
-// TODO: check syntax ^^
+
       // SOUND
+      // TODO: How can a dev use these parameters if they do not have a playback widget to pass audioParams to? 
+      if(this.metronome) console.log('metronome')   // TODO
+      if(this.tempo) {
+        animationOptions.bpm = 120;
+        audioParams.qpm = 120;
+      }
+      if(this.stereo) console.log('stereo')   // TODO
+      if(this.instrument) audioParams.program = 0;
+      if(this.transpose) { 
+        visualOptions.visualTranspose = 0;
+        audioParams.midiTranspose = 0;
+      }
+      if(this.noChords) audioParams.chordsOff = true;
+      if(this.noVoice) audioParams.voicesOff = true;
+
+
       // TIMING
+      // TODO: Currently, both code blocks are triggered by selecting usingCallbacks. Suggestion to present TimingCallbacks and CursorControl as separate checkboxes in the sandbox.
+      const timing = this.usingCallbacks ? 
+`
+var timingCallbacks = new abcjs.TimingCallbacks(visualObj, {});` : '';
+      const cursorControl = this.usingCallbacks ? 
+`
+cursorControl = new synth.CursorControl();` : 
+'';
+
+
       // OTHER
+      if(this.tweak) {
+        synthOptions.sequenceCallback = 'callback to change audio before it is buffered';
+        audioParams.sequenceCallback = 'callback to change audio before it is buffered';
+      }
+      const midi = this.midi ? `ABCJS.renderMidi("midi-download", abc, { generateDownload: true, generateInline: false });` : '';
+      // TODO
+      if(this.playImmediate) { 
+        console.log('playImmediate') 
+      }   
 
-
+      // RETURNED CODE
       return `
-var visualObj = ${abcjs}.renderAbc("${target}", abcString, ${JSON.stringify(visualOptions)});
+var visualOptions = ${JSON.stringify(visualOptions)};
+var audioParams = ${JSON.stringify(audioParams)};
+var cursorControl = null;
+var visualObj = ${abcjs}.renderAbc("${target}", abcString, visualOptions);
 var synthOptions = { visualObj: visualObj[0], ...${JSON.stringify(synthOptions)} };
 
 ${changes}
 
-${cursorControl}
-
 // trigger these on a user gesture: 
 ${widget}
 
-${animation}`;
+${midi} ${animation} ${timing} ${cursorControl}`;
     },
   },
 }
