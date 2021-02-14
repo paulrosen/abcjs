90e69527c13aee81b8319e6bb117c2c34c1d6850
8633brown <toddbrown.8633@yahoo.co.uk>
2021-02-14 15:45:42 -0700

    reformat webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,72 @@
 const pkg = require("./package.json");
 const TerserPlugin = require('terser-webpack-plugin');
 const WebpackBundleAnalyzer = require("webpack-bundle-analyzer")
-	.BundleAnalyzerPlugin;
+  .BundleAnalyzerPlugin;
 
 module.exports = (env, argv) => {
-	const defaults = (argv, type) => {
-		return {
-			output: {
-				library: {
-					amd: 'abcjs',
-					root: 'ABCJS',
-					commonjs: 'abcjs'
-				},
-				libraryTarget: 'umd',
-				globalObject: 'this',
-				filename: argv.mode === 'production' ? `abcjs-${type}-min.js` : `abcjs-${type}.js`,
-			},
-			devtool: argv.mode === 'production' ? false : 'source-map',
-			module: {
-				rules: [
-					{
-						test: /\.js$/,
-						exclude: /node_modules/,
-						use: "babel-loader"
-					}
-				]
-			},
-			optimization:{
-				minimizer: [
-					new TerserPlugin({
-						extractComments: {
-							condition: /^\*\**!/i,
-							banner: makeBanner(type)
-						},
-					}),
-				],
-			}
-		}
-	}
+  const defaults = (argv, type) => {
+    return {
+      output: {
+        library: {
+          amd: 'abcjs',
+          root: 'ABCJS',
+          commonjs: 'abcjs'
+        },
+        libraryTarget: 'umd',
+        globalObject: 'this',
+        filename: argv.mode === 'production' ? `abcjs-${type}-min.js` : `abcjs-${type}.js`,
+      },
+      devtool: argv.mode === 'production' ? false : 'source-map',
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: "babel-loader"
+          }
+        ]
+      },
+      optimization:{
+        minimizer: [
+          new TerserPlugin({
+            extractComments: {
+              condition: /^\*\**!/i,
+              banner: makeBanner(type)
+            },
+          }),
+        ],
+      }
+    }
+  }
 
-	if (env.analyze) {
-		defaults.plugins = [
-			new WebpackBundleAnalyzer()
-		]
-	}
+  if (env.analyze) {
+    defaults.plugins = [
+      new WebpackBundleAnalyzer()
+    ]
+  }
 
-	return [
-		{
-			name: 'basic',
-			entry: `./index.js`,
-			...defaults(argv, 'basic')
-		}, {
-			name: 'plugin',
-			entry: `./static-wrappers/plugin.js`,
-			...defaults(argv, 'plugin')
-		}, {
-			name: 'midi',
-			entry: `./midi.js`,
-			...defaults(argv, 'midi')
-		}
-	]
+  return [
+    {
+      name: 'basic',
+      entry: `./index.js`,
+      ...defaults(argv, 'basic')
+    }, {
+      name: 'plugin',
+      entry: `./static-wrappers/plugin.js`,
+      ...defaults(argv, 'plugin')
+    }, {
+      name: 'midi',
+      entry: `./midi.js`,
+      ...defaults(argv, 'midi')
+    }
+  ]
 };
 
 function makeBanner(type) {
-	const outputFilename = `abcjs_${type}-min.js`;
-	const banner1 = `abcjs_${type} v${pkg.version} Copyright © 2009-2021 Paul Rosen and Gregory Dyke (https://abcjs.net) */`;
-	const banner2 = `/*! midi.js Copyright © Michael Deal (http://mudcu.be) */`;
-	const banner3 = `/*! For license information please see ${outputFilename}.LICENSE`;
-	const hasMidi = type === 'midi';
-	return hasMidi ? `${banner1}\n${banner2}\n${banner3}` : `${banner1}\n${banner3}`;
+  const outputFilename = `abcjs_${type}-min.js`;
+  const banner1 = `abcjs_${type} v${pkg.version} Copyright © 2009-2021 Paul Rosen and Gregory Dyke (https://abcjs.net) */`;
+  const banner2 = `/*! midi.js Copyright © Michael Deal (http://mudcu.be) */`;
+  const banner3 = `/*! For license information please see ${outputFilename}.LICENSE`;
+  const hasMidi = type === 'midi';
+  return hasMidi ? `${banner1}\n${banner2}\n${banner3}` : `${banner1}\n${banner3}`;
 }
