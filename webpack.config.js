6a896468affa07409f232e9b297dc890b53b72d9
8633brown <toddbrown.8633@yahoo.co.uk>
2021-02-19 07:51:08 -0700

    improve licence banner logic

--- webpack.config.js
+++ webpack.config.js
@@ -68,10 +68,9 @@ module.exports = (env = {} , argv) => {
 };
 
 function makeBanner(type) {
-  const outputFilename = `abcjs_${type}-min.js`;
-  const banner1 = `abcjs_${type} v${pkg.version} Copyright © 2009-2021 Paul Rosen and Gregory Dyke (https://abcjs.net) */`;
-  const banner2 = `/*! midi.js Copyright © Michael Deal (http://mudcu.be) */`;
-  const banner3 = `/*! For license information please see ${outputFilename}.LICENSE`;
-  const hasMidi = type === 'midi';
-  return hasMidi ? `${banner1}\n${banner2}\n${banner3}` : `${banner1}\n${banner3}`;
+  let banner = `abcjs_${type} v${pkg.version} Copyright © 2009-2021 Paul Rosen and Gregory Dyke (https://abcjs.net) */\n`
+  if (type === 'midi') {
+    banner += `/*! midi.js Copyright © Michael Deal (http://mudcu.be) */\n`;
+  }
+  return banner + `/*! For license information please see abcjs_${type}.LICENSE`;
 }
