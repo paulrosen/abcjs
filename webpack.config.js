b3860bdffb8cdee7f38cb97618c79593785797c5
8633brown <toddbrown.8633@yahoo.co.uk>
2021-05-27 09:06:52 -0600

    implement typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,99 @@
 const pkg = require("./package.json");
 const TerserPlugin = require('terser-webpack-plugin');
 const WebpackBundleAnalyzer = require("webpack-bundle-analyzer")
-  .BundleAnalyzerPlugin;
+	.BundleAnalyzerPlugin;
 
 module.exports = (env = {} , argv) => {
-  const defaults = (argv, type) => {
-    const config = {
-      target: ['web', 'es5'],
-      output: {
-        library: {
-          amd: 'abcjs',
-          root: 'ABCJS',
-          commonjs: 'abcjs'
-        },
-        libraryTarget: 'umd',
-        globalObject: 'this',
-        filename: argv.mode === 'development' ? `abcjs-${type}.js` : `abcjs-${type}-min.js`,
-      },
-      devtool: argv.mode === 'development' ? 'source-map' : false,
-      module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader",
-              options: {
-                cacheDirectory: (argv.mode === 'development'),
-                presets: [
-                  [
-                    '@babel/preset-env',
-                    {
-                      debug: false,
-                      corejs: '3.9',
-                      useBuiltIns: 'usage',
-                      targets: 'defaults, ie >= 9, safari >= 5.1'
-                    }
-                  ]
-                ]
-              }
-            }
-          }
-        ],
-      },
-      mode: 'production',
-      optimization:{
-        minimizer: [
-          new TerserPlugin({
-            extractComments: {
-              filename: '[file].LICENSE',
-              condition: /^\*\**!/i,
-              banner: makeBanner(type)
-            },
-          }),
-        ],
-      }
-    }
+	const defaults = (argv, type) => {
+		const config = {
+			target: ['web', 'es5'],
+			output: {
+				library: {
+					amd: 'abcjs',
+					root: 'ABCJS',
+					commonjs: 'abcjs'
+				},
+				libraryTarget: 'umd',
+				globalObject: 'this',
+				filename: argv.mode === 'development' ? `abcjs-${type}.js` : `abcjs-${type}-min.js`,
+			},
+			devtool: argv.mode === 'development' ? 'source-map' : false,
+			module: {
+				rules: [
+					{
+						test: /\.js$/,
+						exclude: /node_modules/,
+						use: [
+							{
+								loader: "babel-loader",
+								options: {
+									sourceType: 'unambiguous',
+									cacheDirectory: (argv.mode === 'development'),
+									presets: [
+										[
+											'@babel/preset-env',
+											{
+												debug: false,
+												corejs: '3.9',
+												useBuiltIns: 'usage',
+												targets: 'defaults, ie >= 9, safari >= 5.1'
+											}
+										]
+									]
+								}
+							},
+						]
+					},
+					{
+						test: /\.ts$/,
+						exclude: /node_modules/,
+						use: [
+							'ts-loader',
+						],
+					}
+				],
+			},
+			resolve: {
+				extensions: ['.ts', '.js'],
+			},
+			mode: 'production',
+			optimization:{
+				minimizer: [
+					new TerserPlugin({
+						extractComments: {
+							filename: '[file].LICENSE',
+							condition: /^\*\**!/i,
+							banner: makeBanner(type)
+						},
+					}),
+				],
+			}
+		}
 
-    if (env.analyze) {
-      config.plugins = [
-        new WebpackBundleAnalyzer({
-          analyzerMode: "static"
-        })
-      ]
-    }
-    return config
-  }
+		if (env.analyze) {
+			config.plugins = [
+				new WebpackBundleAnalyzer({
+					analyzerMode: "static"
+				})
+			]
+		}
+		return config
+	}
 
-  return [
-    {
-      name: 'basic',
-      entry: `./index.js`,
-      ...defaults(argv, 'basic')
-    }, {
-      name: 'plugin',
-      entry: `./plugin.js`,
-      ...defaults(argv, 'plugin')
-    }
-  ]
+	return [
+		{
+			name: 'basic',
+			entry: `./index.ts`,
+			...defaults(argv, 'basic')
+		}, {
+			name: 'plugin',
+			entry: `./plugin.js`,
+			...defaults(argv, 'plugin')
+		}
+	]
 };
 
 function makeBanner(type) {
-  let banner = `abcjs_${type} v${pkg.version} Copyright © 2009-2021 Paul Rosen and Gregory Dyke (https://abcjs.net) */\n`
-  return banner + `/*! For license information please see abcjs_${type}.LICENSE`;
+	let banner = `abcjs_${type} v${pkg.version} Copyright © 2009-2021 Paul Rosen and Gregory Dyke (https://abcjs.net) */\n`
+	return banner + `/*! For license information please see abcjs_${type}.LICENSE`;
 }
