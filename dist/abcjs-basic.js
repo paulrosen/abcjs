e53090479676e112ef61100cbb526b485fca52a4
Jean-Yves Mengant <jymengant.teaser@gmail.com>
2021-08-01 16:55:35 +0200

    Fixing flat & sharp handling + Chords multiple notes  on same string

--- dist/abcjs-basic.js
+++ dist/abcjs-basic.js
@@ -16723,50 +16723,89 @@ function buildPatterns(self) {
 
   return strings;
 }
-/**
- * 
- * @param {} self 
- * @param {*} note 
- * @returns 0 : noAccident ; -1 flat ; +1 sharp
- */
 
+function buildSecond(first) {
+  var seconds = [];
+  seconds[0] = [];
+  var strings = first.strings;
 
-function accidentalCheck(self, note) {
-  var accidentals = self.accidentals;
-  var n = note.charAt(0).toUpperCase();
-  var returned = 0;
-
-  if (accidentals) {
-    for (ai = 0; ai < accidentals.length; ai++) {
-      var acc = accidentals[ai];
-
-      if (acc.note.toUpperCase() == n) {
-        if (acc.acc == "flat") {
-          returned = -1;
-        } else {
-          returned = 1;
-        }
-      }
-    }
+  for (iii = 1; iii < strings.length; iii++) {
+    seconds[iii] = strings[iii - 1];
   }
 
-  return returned;
+  return seconds;
 }
 
-function toNumber(self, note) {
-  var num = -1;
-  var str = 0;
+function checkNote(note) {
+  var isFlat = false;
+  var newNote = note;
+  var isSharp = false;
+  var isAltered = false;
   var acc = 0;
 
-  while (str < self.strings.length) {
-    num = self.strings[str].indexOf(note);
+  if (note.startsWith('_')) {
+    isFlat = true;
+    acc = -1;
+  } else if (note.startsWith('^')) {
+    isSharp = true;
+    acc = +1;
+  }
 
-    if (num != -1) {
-      acc = accidentalCheck(self, note);
-      return {
-        num: num + acc,
-        str: str
-      };
+  isAltered = isFlat || isSharp;
+
+  if (isAltered) {
+    newNote = note.slice(1);
+  }
+
+  return {
+    'isAltered': isAltered,
+    'isSharp': isSharp,
+    'isFlat': isFlat,
+    'note': newNote,
+    'acc': acc
+  };
+}
+
+function noteToNumber(self, nNote, stringNumber, secondPosition) {
+  var note = checkNote(nNote);
+  var strings = self.strings;
+
+  if (secondPosition) {
+    strings = secondPosition;
+  }
+
+  num = strings[stringNumber].indexOf(note.note);
+
+  if (num != -1) {
+    if (secondPosition) {
+      num += 7;
+    }
+
+    if (note.isFlat && num == 0) {
+      // flat on 0 pos => previous string Fifth position
+      str = stringNumber + 1;
+      num = 7;
+    }
+
+    return {
+      num: num + note.acc,
+      str: stringNumber,
+      name: nNote
+    };
+  }
+
+  return null;
+}
+
+function toNumber(self, nNote) {
+  var num = null;
+  var str = 0;
+
+  while (str < self.strings.length) {
+    num = noteToNumber(self, nNote, str);
+
+    if (num) {
+      return num;
     }
 
     str++;
@@ -16775,12 +16814,58 @@ function toNumber(self, note) {
   return null; // not found
 }
 
+function sameString(self, chord) {
+  for (jjjj = 0; jjjj < chord.length - 1; jjjj++) {
+    var curPos = chord[jjjj];
+    var nextPos = chord[jjjj + 1];
+
+    if (curPos.str == nextPos.str) {
+      // same String
+      // => change lower pos 
+      if (curPos.str == self.strings.length - 1) {
+        return 'Invalid tab Chord position for instrument';
+      } // change lower pitch on lowest string
+
+
+      if (nextPos.num < curPos.num) {
+        nextPos.str++;
+        nextPos = noteToNumber(self, nextPos.name, nextPos.str, self.secondPos);
+      } else {
+        curPos.str++;
+        curPos = noteToNumber(self, curPos.name, curPos.str, self.secondPos);
+      }
+
+      if (nextPos == null || curPos == null) {
+        return "Can't map tab Chord position for instrument";
+      } // update table
+
+
+      chord[jjjj] = curPos;
+      chord[jjjj + 1] = nextPos;
+    }
+  }
+}
+
+function handleChordNotes(self, notes) {
+  retNotes = [];
+
+  for (iiii = 0; iiii < notes.length; iiii++) {
+    var curPos = toNumber(self, notes[iiii].name);
+    retNotes.push(curPos);
+  }
+
+  var error = sameString(self, retNotes);
+  return retNotes;
+}
+
 StringPatterns.prototype.notesToNumber = function (notes, graces) {
   if (notes) {
     var retNotes = [];
 
-    for (iiii = 0; iiii < notes.length; iiii++) {
-      retNotes.push(toNumber(this, notes[iiii].name));
+    if (notes.length > 1) {
+      retNotes = handleChordNotes(this, notes);
+    } else {
+      retNotes.push(toNumber(this, notes[0].name));
     }
 
     var retGraces = null;
@@ -16788,7 +16873,7 @@ StringPatterns.prototype.notesToNumber = function (notes, graces) {
     if (graces) {
       retGraces = [];
 
-      for (iiii = 0; iiii < notes.length; iiii++) {
+      for (iiii = 0; iiii < graces.length; iiii++) {
         retGraces.push(toNumber(this, graces[iiii].name));
       }
     }
@@ -16808,8 +16893,9 @@ StringPatterns.prototype.toString = function () {
 
 function StringPatterns(tuning) {
   this.tuning = tuning;
-  this.accidentals = null;
-  this.strings = buildPatterns(this);
+  this.strings = buildPatterns(this); // second position pattern per string
+
+  this.secondPos = buildSecond(this);
 }
 
 module.exports = StringPatterns;
