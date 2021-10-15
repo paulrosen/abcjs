89e260b7a300cc4d9e927fa5bb8bb5e109cd3b9c
Paul Rosen <paul@paulrosen.net>
2021-10-14 23:57:13 -0400

    Clean up tests.

--- tests/parse/book_parser.test.js
+++ tests/parse/book_parser.test.js
@@ -1,5 +1,3 @@
-var assert = require('chai').assert
-var Tunebook = require("../../src/api/abc_tunebook")
 /**
  * http://abcnotation.com/wiki/abc:standard:v2.1#xreference_number
  * http://abcnotation.com/wiki/abc:standard:v2.1#ttune_title
@@ -8,38 +6,38 @@ var Tunebook = require("../../src/api/abc_tunebook")
 
 describe("Book Parser function", function () {
 	it("parses a single tune", () => {
-		Tunebook.TuneBook("X:43\nT: example")
-		assert.equal(Tunebook.tunes.length, 1)
-		assert.equal(Tunebook.tunes[0].id, "43")
-		assert.equal(Tunebook.tunes[0].title, "example")
+		var tunebook = new abcjs.TuneBook("X:43\nT: example")
+		chai.assert.equal(tunebook.tunes.length, 1)
+		chai.assert.equal(tunebook.tunes[0].id, "43")
+		chai.assert.equal(tunebook.tunes[0].title, "example")
 	})
 
 	it("parses a single tune with no title", () => {
-		Tunebook.TuneBook("X:43\nT:")
-		assert.equal(Tunebook.tunes.length, 1)
-		assert.equal(Tunebook.tunes[0].id, "43")
-		assert.equal(Tunebook.tunes[0].title, "")
+		var tunebook = new abcjs.TuneBook("X:43\nT:")
+		chai.assert.equal(tunebook.tunes.length, 1)
+		chai.assert.equal(tunebook.tunes[0].id, "43")
+		chai.assert.equal(tunebook.tunes[0].title, "")
 	})
 
 	it("parses multiple tunes", () => {
-		Tunebook.TuneBook("X:1\nT: a\n\nX:2\n\nX:3\nT: c")
-		assert.equal(Tunebook.tunes.length, 3)
-		assert.equal(Tunebook.tunes[0].id, "1")
-		assert.equal(Tunebook.tunes[0].title, "a")
-		assert.equal(Tunebook.tunes[1].id, "2")
-		assert.equal(Tunebook.tunes[1].title, "")
-		assert.equal(Tunebook.tunes[2].id, "3")
-		assert.equal(Tunebook.tunes[2].title, "c")
+		var tunebook = new abcjs.TuneBook("X:1\nT: a\n\nX:2\n\nX:3\nT: c")
+		chai.assert.equal(tunebook.tunes.length, 3)
+		chai.assert.equal(tunebook.tunes[0].id, "1")
+		chai.assert.equal(tunebook.tunes[0].title, "a")
+		chai.assert.equal(tunebook.tunes[1].id, "2")
+		chai.assert.equal(tunebook.tunes[1].title, "")
+		chai.assert.equal(tunebook.tunes[2].id, "3")
+		chai.assert.equal(tunebook.tunes[2].title, "c")
 	})
 
 	it("collects directives in string header", () => {
-		Tunebook.TuneBook("%% example\nT: wed\n%%example\nX:1")
-		assert.equal(Tunebook.header, "%% example\n%%example\n")
+		var tunebook = new abcjs.TuneBook("%% example\nT: wed\n%%example\nX:1")
+		chai.assert.equal(tunebook.header, "%% example\n%%example\n")
 	})
 
 	it("trims whitespace from the end of a tune", () => {
-		Tunebook.TuneBook("%%example\nX:1\nT: a\n\n\n\n\n\nX:2\n\n")
-		assert.equal(Tunebook.tunes[0].abc, "%%example\nX:1\nT: a")
-		assert.equal(Tunebook.tunes[0].pure, "X:1\nT: a")
+		var tunebook = new abcjs.TuneBook("%%example\nX:1\nT: a\n\n\n\n\n\nX:2\n\n")
+		chai.assert.equal(tunebook.tunes[0].abc, "%%example\nX:1\nT: a")
+		chai.assert.equal(tunebook.tunes[0].pure, "X:1\nT: a")
 	})
 })
