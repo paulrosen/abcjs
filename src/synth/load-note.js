02435daecf350301202be64967a1d23e80d510f7
Hajime Yamasaki Vukelic <hi@devi.us>
2021-11-25 13:15:33 +0100

    Implement a fix for race conditions during note loading
    This fix addresses the issue with race conditions when multiple
    instances attempt to load the same note. One instance will
    overwrite the resolved AudioBuffer from another instance with the
    'pending' string. This fix addresses this by setting an instance
    of a Promise instead of strings that represent the status, and
    modifies putNote() so that it makes no assumptions about the
    status, and instead simply resolves the promise to obtain the
    buffer.
    
    See #721

--- src/synth/load-note.js
+++ src/synth/load-note.js
@@ -2,74 +2,40 @@
 // url = the base url for the soundfont
 // instrument = the instrument name (e.g. "acoustic_grand_piano")
 // name = the pitch name (e.g. "A3")
-var soundsCache = require('./sounds-cache');
+var soundsCache = require("./sounds-cache");
 
-var getNote = function(url, instrument, name, audioContext) {
-	return new Promise(function (resolve, reject) {
-		if (!soundsCache[instrument])
-			soundsCache[instrument] = {};
-		var instrumentCache = soundsCache[instrument];
+var getNote = function (url, instrument, name, audioContext) {
+	if (!soundsCache[instrument]) soundsCache[instrument] = {};
+	var instrumentCache = soundsCache[instrument];
 
-		if (instrumentCache[name] === 'error') {
-			return resolve({instrument: instrument, name: name, status: "error", message: "Unable to load sound font" + ' ' + url + ' ' + instrument + ' ' + name });
-		}
-		if (instrumentCache[name] === 'pending') {
-			return resolve({instrument: instrument, name: name, status: "pending"});
-		}
-		if (instrumentCache[name]) {
-			return resolve({instrument: instrument, name: name, status: "cached"});
-		}
-
-		// if (this.debugCallback)
-		// 	this.debugCallback(`Loading sound: ${instrument} ${name}`);
-		instrumentCache[name] = "pending"; // This can be called in parallel, so don't call it a second time before the first one has loaded.
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', url+instrument+'-mp3/'+name+'.mp3', true);
-		xhr.responseType = 'arraybuffer';
-
-		var self = this;
-		function onSuccess(audioBuffer) {
-			instrumentCache[name] = audioBuffer;
-			// if (self.debugCallback)
-			// 	self.debugCallback(`Sound loaded: ${instrument} ${name} ${url}`);
-			resolve({instrument: instrument, name: name, status: "loaded"});
-		}
-
-		function onFailure(error) {
-			error = "Can't decode sound. " + url + ' ' + instrument + ' ' + name + ' ' + error;
-			if (self.debugCallback)
-				self.debugCallback(error);
-			return resolve({instrument: instrument, name: name, status: "error", message: error });
-		}
-
-		xhr.onload = function (e) {
-			if (this.status === 200) {
-				try {
-					var promise = audioContext.decodeAudioData(this.response, onSuccess, onFailure);
-					// older browsers only have the callback. Newer ones will report an unhandled
-					// rejection if catch isn't handled so we need both. We don't need to report it twice, though.
-					if (promise && promise.catch)
-						promise.catch(function () {});
-				} catch(error) {
-					reject(error);
+	if (!instrumentCache[name])
+		instrumentCache[name] = new Promise(function (resolve, reject) {
+			var xhr = new XMLHttpRequest();
+			let noteUrl = url + instrument + "-mp3/" + name + ".mp3";
+			xhr.open("GET", noteUrl, true);
+			xhr.responseType = "arraybuffer";
+			xhr.onload = function () {
+				if (xhr.status !== 200) {
+					reject(Error("Can't load sound at " + noteUrl));
+					return
 				}
-			} else {
-				instrumentCache[name] = "error"; // To keep this from trying to load repeatedly.
-				var cantLoadMp3 = "Onload error loading sound: " +  name + " " + url + " " + e.currentTarget.status + " " + e.currentTarget.statusText;
-				if (self.debugCallback)
-					self.debugCallback(cantLoadMp3);
-				return resolve({instrument: instrument, name: name, status: "error", message: cantLoadMp3 });
-			}
-		};
-		xhr.addEventListener("error", function () {
-			instrumentCache[name] = "error"; // To keep this from trying to load repeatedly.
-			var cantLoadMp3 = "Error in loading sound: " + " " + url;
-			if (self.debugCallback)
-				self.debugCallback(cantLoadMp3);
-			return resolve({instrument: instrument, name: name, status: "error", message: cantLoadMp3 });
-		}, false);
-		xhr.send();
-	});
+				var maybePromise = audioContext.decodeAudioData(xhr.response, resolve, function () {
+					reject(Error("Can't decode sound at " + noteUrl));
+				});
+				// In older browsers `BaseAudioContext.decodeAudio()` did not return a promise
+				if (maybePromise && typeof maybePromise.catch === "function") maybePromise.catch(reject);
+			};
+			xhr.onerror = function () {
+				reject(Error("Can't load sound at " + noteUrl));
+			};
+			xhr.send();
+		})
+			.catch(err => {
+				console.error("Didn't load note", instrument, name, ":", err.message);
+				throw err;
+			});
+
+	return instrumentCache[name];
 };
 
 module.exports = getNote;
