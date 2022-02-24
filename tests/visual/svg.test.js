a601050cb1598def1eacf14a042a664aa8de6b6b
Paul Rosen <paul@paulrosen.net>
2022-02-23 20:03:07 -0500

    Don't add extra padding when creating minimal music (like one note)

--- tests/visual/svg.test.js
+++ tests/visual/svg.test.js
@@ -1,24 +1,17 @@
 describe("SVG Creation", function() {
 	var abcSingleNote = "X:1\n" +
 		"L:1/4\n" +
-		"%%staffwidth 12\n" +
+		"%%staffwidth 5\n" +
 		"%%musicspace 0\n" +
 		"K:C clef=none\n" +
 		"V:all stem=up\n" +
-		"A4\n"
-	var abcSingleRest = "X:1\n" +
-		"L:1/4\n" +
-		"%%staffwidth 12\n" +
-		"%%musicspace 0\n" +
-		"K:C clef=none\n" +
-		"V:all stem=up\n" +
-		"z\n"
+		"B4\n"
 	var abcTimeSigList = "X:1\n" +
 		"L:1/4\n" +
 		"%%staffwidth 12\n" +
 		"%%musicspace 0\n" +
 		"K:C clef=none stafflines=0\n" +
-		"[M:2/4]y[M:3/4]y[M:4/4]y/4\n"
+		"[M:2/4]y[M:3/4]y[M:4/4]\n"
 	var abc128Group = "X:1\n" +
 		"L:1/4\n" +
 		"M:12/8\n" +
@@ -37,10 +30,6 @@ describe("SVG Creation", function() {
 		testSvg(abcSingleNote)
 		testCenter("note")
 	})
-	it("single-rest-compact", function() {
-		testSvg(abcSingleRest)
-		testCenter("rests.quarter")
-	})
 	it("time-sig-list-compact", function() {
 		testSvg(abcTimeSigList)
 		testCenterGroup()
@@ -51,7 +40,7 @@ describe("SVG Creation", function() {
 	})
 
 	function testSvg(abc) {
-		abcjs.renderAbc("paper", abc, { add_classes: true });
+		abcjs.renderAbc("paper", abc, { add_classes: true, showDebug: [ 'box', 'grid' ], paddingleft: 0, paddingtop: 0, paddingright: 0, paddingbottom: 0 });
 	}
 
 	function testCenter(nameType) {
@@ -65,22 +54,24 @@ describe("SVG Creation", function() {
 		var box = item.getBBox()
 		var xCenter = box.x + box.width / 2;
 		var yCenter = box.y + box.height / 2;
-		chai.assert.equal(xCenter,svgXCenter, "X coordinate not centered")
-		chai.assert.equal(yCenter,svgYCenter, "Y coordinate not centered")
+		var xDiff = Math.abs(xCenter - svgXCenter)
+		var yDiff = Math.abs(yCenter - svgYCenter)
+		chai.assert.isBelow(xDiff,1, "X coordinate not centered (exp: " + svgXCenter + " act: " + xCenter + ")")
+		chai.assert.isBelow(yDiff,1, "Y coordinate not centered (exp: " + svgYCenter + " act: " + yCenter + ")")
 	}
 
 	function testCenterGroup() {
 		var svg = document.querySelector("#paper svg")
 		var width = svg.getAttribute("width")
 		var height = svg.getAttribute("height")
-		var svgXCenter = width/2
-		var svgYCenter = height/2
+		var svgXCenter = Math.round(width/2)
+		var svgYCenter = Math.round(height/2)
 
 		var items = svg.querySelectorAll('g')
 		var box1 = items[0].getBBox()
 		var box2 = items[items.length-1].getBBox()
-		var xCenter = box1.x + (box1.x + box2.x) / 2;
-		var yCenter = box1.y + box1.height / 2;
+		var xCenter = Math.round(box1.x + (box1.x + box2.x + box2.width) / 2);
+		var yCenter = Math.round(box1.y + box1.height / 2);
 		chai.assert.equal(xCenter,svgXCenter, "X coordinate not centered")
 		chai.assert.equal(yCenter,svgYCenter, "Y coordinate not centered")
 	}
