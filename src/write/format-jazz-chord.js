350a91a1c0b02fd263daedda1311a857766182bc
Paul Rosen <paul@paulrosen.net>
2022-04-03 18:49:11 -0400

    Fix bass note in jazz chord when it contains a sharp or flat

--- src/write/format-jazz-chord.js
+++ src/write/format-jazz-chord.js
@@ -5,7 +5,7 @@ function formatJazzChord(chordString) {
 	for (var i = 0; i < lines.length; i++) {
 		var chord = lines[i];
 		// If the chord isn't in a recognizable format then just skip the formatting.
-		var reg = chord.match(/^([ABCDEFG][♯♭]?)?([^\/]+)?(\/[ABCDEFG][#b]?)?/);
+		var reg = chord.match(/^([ABCDEFG][♯♭]?)?([^\/]+)?(\/[ABCDEFG][#b♯♭]?)?/);
 		if (reg)
 			lines[i] = (reg[1]?reg[1]:'') + "\x03" + (reg[2]?reg[2]:'')  + "\x03" + (reg[3]?reg[3]:'');
 	}
