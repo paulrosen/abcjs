1176c5bef2dd671d021bcd15eeefccf143fd5de7
Paul Rosen <paul@paulrosen.net>
2023-04-16 17:44:32 -0400

    prepare 6.2.2 release

--- dist/abcjs-basic.js
+++ dist/abcjs-basic.js
@@ -16632,6 +16632,7 @@ TabRenderer.prototype.doLayout = function () {
   this.tabStaff.voices = [];
   for (var ii = 0; ii < nbVoices; ii++) {
     var tabVoice = new VoiceElement(0, 0);
+    if (ii > 0) tabVoice.duplicate = true;
     var nameHeight = buildTabName(this, tabVoice) / spacing.STEP;
     nameHeight = Math.max(nameHeight, 1); // If there is no label for the tab line, then there needs to be a little padding
     staffGroup.staffs[this.staffIndex].top += nameHeight;
@@ -21574,7 +21575,7 @@ function printLine(renderer, x1, x2, y, klass, name, dy) {
       y1: y,
       y2: y,
       stroke: renderer.foregroundColor,
-      'stroke-width': Math.abs(dy)
+      'stroke-width': Math.abs(dy * 2)
     };
     if (klass) attr['class'] = klass;
     if (name) attr['data-name'] = name;
@@ -25541,7 +25542,7 @@ module.exports = Svg;
   \********************/
 /***/ (function(module) {
 
-var version = '6.2.1';
+var version = '6.2.2';
 module.exports = version;
 
 /***/ })
