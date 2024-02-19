a925bfba5051a5db80273ed9f94622e4a25be986
Meriadoc Lawes <mlawes@gmail.com>
2024-02-19 07:35:08 +0000

    change return

--- src/api/abc_tunebook.js
+++ src/api/abc_tunebook.js
@@ -41,7 +41,7 @@ var tunebook = {};
 
 	TuneBook.prototype.getTunesByType = function(type) {
 		if (type !== "none") {
-			return this.tunes.filter((tune) => tune.type.toLowerCase() === type.toLowerCase())
+			this.tunes.filter((tune) => tune.type.toLowerCase() === type.toLowerCase())
 		}
 		
 		return this;
