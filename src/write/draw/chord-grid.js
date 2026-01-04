2372ca956bd55b16ed10de003c6ecf617198c537
paulrosen <junk@paulrosen.net>
2026-01-04 14:50:03 -0500

    Some visual tweaks for the chord grid

--- src/write/draw/chord-grid.js
+++ src/write/draw/chord-grid.js
@@ -50,6 +50,9 @@ function drawChordGrid(renderer, parts, leftMargin, pageWidth, fonts) {
 							if (!measure.noBorder) {
 								renderer.paper.rect({x: leftMargin + barNum * colWidth, y: renderer.y, width: colWidth, height: extraTop + ROW_HEIGHT})
 								renderer.paper.rect({x: leftMargin + barNum * colWidth + RECT_WIDTH, y: renderer.y + RECT_WIDTH, width: colWidth - RECT_WIDTH * 2, height: extraTop + ROW_HEIGHT - RECT_WIDTH * 2})
+								if (extraTop) {
+									renderer.paper.rectBeneath({x: leftMargin + barNum * colWidth, y: renderer.y, width: colWidth, height: extraTop, fill: '#e8e8e8', stroke: 'none'})
+								}
 								let repeatLeft = 0
 								let repeatRight = 0
 								const top = renderer.y
@@ -81,6 +84,34 @@ function drawChordGrid(renderer, parts, leftMargin, pageWidth, fonts) {
 				break;
 		}
 	})
+}
+
+function drawPercent(renderer, x, y, offset) {
+	var lineX1 = x - 10
+	var lineX2 = x + 10
+	var lineY1 = y + 10
+	var lineY2 = y - 10
+	var leftDotX = x - 10
+	var leftDotY = -renderer.yToPitch(offset) + 2
+	var rightDotX = x + 6.5
+	var rightDotY = -renderer.yToPitch(offset) -2.3
+
+	renderer.paper.lineToBack({x1: lineX1, x2: lineX2, y1: lineY1, y2: lineY2, 'stroke-width': '3px', 'stroke-linecap':"round"  })
+
+	printSymbol(renderer, leftDotX, leftDotY, "dots.dot", {
+		scalex: 1,
+		scaley: 1,
+		klass: "",
+		name: "dot"
+	});
+
+	printSymbol(renderer, rightDotX, rightDotY, "dots.dot", {
+		scalex: 1,
+		scaley: 1,
+		klass: "",
+		name: "dot"
+	});
+
 }
 
 function drawRepeat(renderer, x, y1, y2, isStart, offset) {
@@ -141,7 +172,7 @@ function drawAnnotations(renderer, offset, left, annotations, annotationFont) {
 function drawMeasure(renderer, offset, leftMargin, colWidth, lineNum, barNum, chords, chordFont, margin, height, extraTop) {
 	const left = leftMargin + colWidth * barNum
 	if (!chords[1] && !chords[2] && !chords[3])
-		drawSingleChord(renderer, left, offset+extraTop, colWidth-margin, height, chords[0], chordFont)
+		drawSingleChord(renderer, left, offset+extraTop, colWidth-margin, height, chords[0], chordFont, extraTop)
 	else if (!chords[1] && !chords[3])
 		drawTwoChords(renderer, left, offset, colWidth-margin, height, chords[0], chords[2], chordFont, extraTop)
 	else
@@ -152,7 +183,7 @@ function renderChord(renderer, x, y, size, chord, font, maxWidth) {
 	const el = text(renderer, chord, x, y, size, font, null, "abcjs-chord", true)
 	let bb = el.getBBox()
 	let fontSize = size
-	while (bb.width > maxWidth && fontSize >= 16) {
+	while (bb.width > maxWidth && fontSize >= 14) {
 		fontSize -= 2
 		el.setAttribute('font-size', fontSize)
 		bb = el.getBBox()
@@ -162,30 +193,40 @@ function renderChord(renderer, x, y, size, chord, font, maxWidth) {
 const MAX_ONE_CHORD = 34
 const MAX_TWO_CHORDS = 26
 const MAX_FOUR_CHORDS = 20
+const TOP_MARGIN = -3
 
-function drawSingleChord(renderer, left, top, width, height, chord, font) {
-	renderChord(renderer, left+width/2, top+height/2, MAX_ONE_CHORD, chord, font, width)
+function drawSingleChord(renderer, left, top, width, height, chord, font, extraTop) {
+	if (chord === '%')
+		drawPercent(renderer, left+width/2, top+height/2, extraTop+height/2)
+	else
+		renderChord(renderer, left+width/2, top+height/2+TOP_MARGIN, MAX_ONE_CHORD, chord, font, width)
 }
 
 function drawTwoChords(renderer, left, top, width, height, chord1, chord2, font, extraTop) {
 	renderer.paper.lineToBack({x1: left, x2: left+width, y1: top+height+extraTop, y2: top })
-	renderChord(renderer, left+width/4, top+height/4+5+extraTop, MAX_TWO_CHORDS, chord1, font, width/2)
-	renderChord(renderer, left+3*width/4, top+3*height/4+extraTop, MAX_TWO_CHORDS, chord2, font, width/2)
+	renderChord(renderer, left+width/4, top+height/4+5+extraTop+TOP_MARGIN, MAX_TWO_CHORDS, chord1, font, width/2)
+	renderChord(renderer, left+3*width/4, top+3*height/4+extraTop+TOP_MARGIN, MAX_TWO_CHORDS, chord2, font, width/2)
 }
 
 function drawFourChords(renderer, left, top, width, height, chords, font, extraTop) {
-	const MARGIN = 5
+	const MARGIN = 3
 	renderer.paper.lineToBack({x1: left+MARGIN, x2: left+width-MARGIN, y1: top+height/2+extraTop, y2: top+height/2+extraTop })
 	renderer.paper.lineToBack({x1: left+width/2, x2: left+width/2, y1: top+MARGIN+extraTop, y2: top+height-MARGIN+extraTop })
 
 	if (chords[0])
-		renderChord(renderer, left+width/4, top+height/4+2+extraTop, MAX_FOUR_CHORDS, chords[0], font, width / 2)
+		renderChord(renderer, left+width/4, top+height/4+2+extraTop+TOP_MARGIN, MAX_FOUR_CHORDS, shortenChord(chords[0]), font, width / 2)
 	if (chords[1])
-		renderChord(renderer, left+3*width/4, top+height/4+2+extraTop, MAX_FOUR_CHORDS, chords[1], font, width / 2)
+		renderChord(renderer, left+3*width/4, top+height/4+2+extraTop+TOP_MARGIN, MAX_FOUR_CHORDS, shortenChord(chords[1]), font, width / 2)
 	if (chords[2])
-		renderChord(renderer, left+width/4, top+3*height/4+extraTop, MAX_FOUR_CHORDS, chords[2], font, width / 2)
+		renderChord(renderer, left+width/4, top+3*height/4+extraTop+TOP_MARGIN, MAX_FOUR_CHORDS, shortenChord(chords[2]), font, width / 2)
 	if (chords[3])
-		renderChord(renderer, left+3*width/4, top+3*height/4+extraTop, MAX_FOUR_CHORDS, chords[3], font, width / 2)
+		renderChord(renderer, left+3*width/4, top+3*height/4+extraTop+TOP_MARGIN, MAX_FOUR_CHORDS, shortenChord(chords[3]), font, width / 2)
+}
+
+function shortenChord(chord) {
+	if (chord === "No Chord")
+		return "N.C."
+	return chord
 }
 
 function text(renderer, str, x, y, size, font, dataName, klass, alignCenter) {
